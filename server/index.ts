2c2907999155def92c809ee6742a3e9e72d60098
Aleksi Hannula <aleksi.hannula@aalto.fi>
2022-11-08 13:59:29 +0200

    Make backend port configurable via environment variable

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,13 @@
 import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
-const port: number = 3000;
+const parsedPort: number = Number(process.env.AALTO_GRADES_BACKEND_PORT);
+const port: number = isNaN(parsedPort) ? 3000 : parsedPort;
 
 app.get('*', (req: Request, res: Response) => {
   res.send(`Hello ${req.path}`);
 });
 
 app.listen(port, () => {
-  console.log('Hello server');
+  console.log(`Hello server, running on port ${port}`);
 });
