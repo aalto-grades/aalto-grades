06452d9fc4ac47232ebfc02296e1113f9dfb0deb
Aleksi Hannula <aleksi.hannula@aalto.fi>
2022-11-15 13:20:12 +0200

    Implement skeleton route for signup

--- server/auth.ts
+++ server/auth.ts
@@ -1,3 +1,9 @@
-export function validateLogin(username: String, password: String): boolean {
-    return username.toLowerCase() === "aalto" && password === "grades";
+export type PlainPassword = String;
+
+export function validateLogin(username: String, password: PlainPassword): boolean {
+  return username.toLowerCase() === "aalto" && password === "grades";
+}
+
+export function performSignup(username: String, email: String, plainPassword: PlainPassword): boolean {
+  return true;
 }
