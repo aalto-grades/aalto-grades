e6f8e617a8596d2c802195277ed69a5dec1d50a6
Aleksi Hannula <aleksi.hannula@aalto.fi>
2022-11-16 14:42:56 +0200

    Auto-format backend TS

--- server/index.ts
+++ server/index.ts
@@ -6,56 +6,56 @@ const parsedPort = Number(process.env.AALTO_GRADES_BACKEND_PORT);
 const port: number = isNaN(parsedPort) ? 3000 : parsedPort;
 
 interface LoginRequest {
-  username: String,
+  username: string,
   password: PlainPassword,
 }
 
 interface SignupRequest {
-    username: String,
+    username: string,
     password: PlainPassword,
-    email: String,
+    email: string,
     role: UserRole,
 }
 
-const validateUserRole = (role: any): role is UserRole => typeof role === "string" && (
-    role === "Teacher" ||
-    role === "Student" ||
-    role === "Admin"
+const validateUserRole = (role: any): role is UserRole => typeof role === 'string' && (
+  role === 'Teacher' ||
+    role === 'Student' ||
+    role === 'Admin'
 );
 
 const validateLoginFormat = (body: any): body is LoginRequest =>
-    body &&
+  body &&
     body.username &&
     body.password &&
-    typeof body.username === "string" &&
-    typeof body.password === "string";
+    typeof body.username === 'string' &&
+    typeof body.password === 'string';
 
 const validateSignupFormat = (body: any): body is SignupRequest =>
-    body &&
+  body &&
     body.username &&
     body.password &&
     body.email &&
     validateUserRole(body.role) &&
-    typeof body.username === "string" &&
-    typeof body.password === "string" &&
-    typeof body.email === "string";
+    typeof body.username === 'string' &&
+    typeof body.password === 'string' &&
+    typeof body.email === 'string';
 
 
 app.post('/v1/auth/login', express.json(), (req: Request, res: Response) => {
   if (!validateLoginFormat(req.body)) {
     res.status(400);
     return res.send({
-        success: false,
-        error: "Invalid login request format",
+      success: false,
+      error: 'Invalid login request format',
     });
   }
 
   // TODO: Rather use Promises here? Team feedback needed
   validateLogin(req.body.username, req.body.password).then(role => {
-      res.send({
-          success: true,
-          role: role,
-      });
+    res.send({
+      success: true,
+      role: role,
+    });
   }).catch(error => {
     res.status(401);
     res.send({
@@ -69,15 +69,15 @@ app.post('/v1/auth/signup', express.json(), (req: Request, res: Response) => {
   if (!validateSignupFormat(req.body)) {
     res.status(400); 
     return res.send({
-        success: false,
-        error: "Invalid signup request format",
+      success: false,
+      error: 'Invalid signup request format',
     });
   }
 
   performSignup(req.body.username, req.body.email, req.body.password, req.body.role).then(() => {
     res.send({
-        success: true
-      });
+      success: true
+    });
   }).catch(error => {
     // 403 or 400 or 500? The Promise architecture with appropriate rejections should
     // carry this info
