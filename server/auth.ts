84386e42f39034e8baab0405607e55363840e713
Aleksi Hannula <aleksi.hannula@aalto.fi>
2022-11-16 14:42:56 +0200

    Auto-format backend TS

--- server/auth.ts
+++ server/auth.ts
@@ -1,17 +1,17 @@
-export type PlainPassword = String;
-export type UserRole = "Teacher" | "Student" | "Admin";
+export type PlainPassword = string;
+export type UserRole = 'Teacher' | 'Student' | 'Admin';
 
-export function validateLogin(username: String, password: PlainPassword): Promise<UserRole> {
+export function validateLogin(username: string, password: PlainPassword): Promise<UserRole> {
   return new Promise((resolve, reject) => {
-      if (username.toLowerCase() === "aalto" && password === "grades") {
-          resolve("Admin");
-      } else {
-          reject("Invalid credentials");
-      }
+    if (username.toLowerCase() === 'aalto' && password === 'grades') {
+      resolve('Admin');
+    } else {
+      reject('Invalid credentials');
+    }
   });
 }
 
-export function performSignup(username: String, email: String, plainPassword: PlainPassword, role: UserRole): Promise<void> {
+export function performSignup(username: string, email: string, plainPassword: PlainPassword, role: UserRole): Promise<void> {
   return new Promise((resolve, reject) => {
     resolve();
   });
