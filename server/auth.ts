95c5c6e3b737b6de2dc7a83e35c567bda93ac07a
Aleksi Hannula <aleksi.hannula@aalto.fi>
2022-11-16 14:34:33 +0200

    Start using Promises for auth backend points, add support for user roles

--- server/auth.ts
+++ server/auth.ts
@@ -1,9 +1,18 @@
 export type PlainPassword = String;
+export type UserRole = "Teacher" | "Student" | "Admin";
 
-export function validateLogin(username: String, password: PlainPassword): boolean {
-  return username.toLowerCase() === "aalto" && password === "grades";
+export function validateLogin(username: String, password: PlainPassword): Promise<UserRole> {
+  return new Promise((resolve, reject) => {
+      if (username.toLowerCase() === "aalto" && password === "grades") {
+          resolve("Admin");
+      } else {
+          reject("Invalid credentials");
+      }
+  });
 }
 
-export function performSignup(username: String, email: String, plainPassword: PlainPassword): boolean {
-  return true;
+export function performSignup(username: String, email: String, plainPassword: PlainPassword, role: UserRole): Promise<void> {
+  return new Promise((resolve, reject) => {
+    resolve();
+  });
 }
