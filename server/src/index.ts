13a9dedcd2e35fdbf4abe0042a186e2843c19637
Aleksi Hannula <aleksi.hannula@aalto.fi>
2022-11-23 15:42:55 +0200

    Add basic DB test endpoint, listing all course data

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,10 +4,27 @@
 
 import express, { Application, Request, Response } from 'express';
 import { connectToDatabase } from './src/database';
+import Course from './src/database/models/course';
 
 const app: Application = express();
 const parsedPort = Number(process.env.AALTO_GRADES_BACKEND_PORT);
 const port: number = isNaN(parsedPort) ? 3000 : parsedPort;
+
+app.get('/v1/test/db', async (req: Request, res: Response) => {
+  try {
+    res.json({
+      success: true,
+      data: await Course.findAll(),
+    });
+  } catch (err) {
+    res.status(500);
+    console.log('DB test error:', err);
+    res.json({
+      success: false,
+      error: '',
+    });
+  }
+});
 
 app.get('*', (req: Request, res: Response) => {
   res.send(`Hello ${req.path}`);
