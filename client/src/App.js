f339937c9585c116a7af3e4235a40e3475fb0f81
Aleksi Hannula <aleksi.hannula@aalto.fi>
2022-12-06 19:06:43 +0200

    Remove traces of old Hello world component

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import PrivateRoute from './components/auth/PrivateRoute';
-import Main from './components/Main';
 import Info from './components/Info';
 import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
@@ -72,7 +71,7 @@ function App() {
             <Route path='/login' element={<Login/>} />
             <Route path='/signup' element={<Signup/>} />
             <Route element={<PrivateRoute/>}>
-              <Route path='/' element={<Main/>} />
+              <Route path='/' element={<FrontPage/>} />
               <Route path='info' element={<Info />} />
             </Route>
             <Route path='/course-view/:courseCode' element={<CourseView/>}/>  {/* Add nested routes when needed */}
