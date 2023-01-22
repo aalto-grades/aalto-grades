2df292e9f05ec8d5bef09977fe4d887107a72d96
jj-stigell <63514007+jj-stigell@users.noreply.github.com>
2023-01-22 08:49:23 +0200

    add license info

--- server/src/types/sisu.ts
+++ server/src/types/sisu.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 The Aalto Grades Developers
+//
+// SPDX-License-Identifier: MIT
+
 import { LocalizedString } from '../controllers/course';
 
 export interface Organizations {
