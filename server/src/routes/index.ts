6a609c67dff9cdaa543b31e4f4eda25275987bbd
Tonits <tony.m.lindstrom@outlook.com>
2023-01-25 10:28:12 +0200

    small cleanup

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -11,6 +11,7 @@ import { getAllCourseInstances } from '../controllers/course';
 export const router: Router = Router();
 
 router.get('/v1/user/:userId/courses', getUserCourses);
+router.get('/v1/courses/:courseId/instances', getAllCourseInstances);
 
 // Sisu API routes
 router.get('/v1/courses/sisu/:courseId', fetchAllInstancesFromSisu);
@@ -23,8 +24,6 @@ router.get('/v1/test/db', testDbFindAllUsers);
 
 // TODO: remove this test endpoint after working endpoint has been added
 router.get('/v1/test/db/courses/:langId', testDbFindAllCourses);
-
-router.get('/v1/courses/:courseId/instances', getAllCourseInstances);
 
 router.get('*', (req: Request, res: Response) => {
   res.send(`Hello ${req.path}`);
