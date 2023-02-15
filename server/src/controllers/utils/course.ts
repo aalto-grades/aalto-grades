f549bc4ad13383154238f10f70233285dd533e6a
Jere Stigell <jj.stigell@gmail.com>
2023-02-15 18:16:17 +0200

    Api errors to utli functions

---
+++ server/src/controllers/utils/course.ts
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: 2023 The Aalto Grades Developers
+//
+// SPDX-License-Identifier: MIT
+
+import Course from '../../database/models/course';
+
+import { ApiError } from '../../middleware/errorHandler';
+import { HttpCode } from '../../types/httpCode';
+
+/**
+ * Finds a course by its ID.
+ * @param {number} courseId - The ID of the course.
+ * @returns {Promise<Course>} - The found course model object.
+ * @throws {Error} - If the course is not found, it throws an error with a message
+ * indicating the missing course with the specific ID.
+ */
+export async function findCourseById(courseId: number): Promise<Course> {
+  const course: Course | null = await Course.findByPk(courseId);
+  if (!course) {
+    throw new ApiError(`course with ID ${courseId} not found`, HttpCode.NotFound);
+  }
+  return course;
+}
