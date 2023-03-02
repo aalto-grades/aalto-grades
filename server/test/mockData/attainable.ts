0268b0d73199d3e185170a5b2979f074c1f8a131
Jere Stigell <jj.stigell@gmail.com>
2023-03-02 17:25:15 +0700

    REUSE

--- server/test/mockData/attainable.ts
+++ server/test/mockData/attainable.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 The Aalto Grades Developers
+//
+// SPDX-License-Identifier: MIT
+
 import { AttainableData } from '../../src/types/course';
 
 export const mockAttainables: AttainableData = {
