3f067dc8db16c1b244dc19ff02342019c3d95146
Jere Stigell <jj.stigell@gmail.com>
2023-03-03 11:41:29 +0700

    update mockdata styling

--- server/test/mockData/attainable.ts
+++ server/test/mockData/attainable.ts
@@ -8,63 +8,75 @@ export const mockAttainables: AttainableData = {
   name: '2 optional exercises',
   executionDate: new Date(2023, 9, 1),
   expiryDate: new Date(2024, 8, 14),
-  subAssignments: [
-    { name: 'Exercise 1',
+  subAttainables: [
+    {
+      name: 'Exercise 1',
       executionDate: new Date(2023, 9, 1),
       expiryDate: new Date(2024, 8, 14),
-      subAssignments: [],
+      subAttainables: [],
     },
-    { name: 'Exercise 2',
+    {
+      name: 'Exercise 2',
       executionDate: new Date(2023, 9, 1),
       expiryDate: new Date(2024, 8, 14),
-      subAssignments: [
-        { name: 'Exercise 2.1',
+      subAttainables: [
+        {
+          name: 'Exercise 2.1',
           executionDate: new Date(2023, 9, 1),
           expiryDate: new Date(2024, 8, 14),
-          subAssignments: [
-            { name: 'Exercise 2.1.1',
+          subAttainables: [
+            {
+              name: 'Exercise 2.1.1',
               executionDate: new Date(2023, 9, 1),
               expiryDate: new Date(2024, 8, 14),
-              subAssignments: [],
+              subAttainables: [],
             },
-            { name: 'Exercise 2.1.2',
+            {
+              name: 'Exercise 2.1.2',
               executionDate: new Date(2023, 9, 1),
               expiryDate: new Date(2024, 8, 14),
-              subAssignments: [],
+              subAttainables: [],
             }
           ],
         },
-        { name: 'Exercise 2.2',
+        {
+          name: 'Exercise 2.2',
           executionDate: new Date(2023, 9, 1),
           expiryDate: new Date(2024, 8, 14),
-          subAssignments: [
-            { name: 'Exercise 2.2.1',
+          subAttainables: [
+            {
+              name: 'Exercise 2.2.1',
               executionDate: new Date(2023, 9, 1),
               expiryDate: new Date(2024, 8, 14),
-              subAssignments: [],
+              subAttainables: [],
             },
-            { name: 'Exercise 2.2.2',
+            {
+              name: 'Exercise 2.2.2',
               executionDate: new Date(2023, 9, 1),
               expiryDate: new Date(2024, 8, 14),
-              subAssignments: [
-                { name: 'Exercise 2.2.2.1',
+              subAttainables: [
+                {
+                  name: 'Exercise 2.2.2.1',
                   executionDate: new Date(2023, 9, 1),
                   expiryDate: new Date(2024, 8, 14),
-                  subAssignments: [],
+                  subAttainables: [],
                 },
-                { name: 'Exercise 2.2.2.2',
+                {
+                  name: 'Exercise 2.2.2.2',
                   executionDate: new Date(2023, 9, 1),
                   expiryDate: new Date(2024, 8, 14),
-                  subAssignments: [
-                    { name: 'Exercise 2.2.2.2.1',
+                  subAttainables: [
+                    {
+                      name: 'Exercise 2.2.2.2.1',
                       executionDate: new Date(2023, 9, 1),
                       expiryDate: new Date(2024, 8, 14),
-                      subAssignments: [],
+                      subAttainables: [],
                     },
-                    { name: 'Exercise 2.2.2.2.2',
+                    {
+                      name: 'Exercise 2.2.2.2.2',
                       executionDate: new Date(2023, 9, 1),
                       expiryDate: new Date(2024, 8, 14),
-                      subAssignments: [],
+                      subAttainables: [],
                     }
                   ],
                 }
