303c5a25fc9af7fca664181a3c7488c2507991a9
viivi-uhari <viivi.uhari@gmail.com>
2023-03-16 10:36:12 +0200

    Improve tests

--- client/src/tests/CreateAssignmentView.test.js
+++ client/src/tests/CreateAssignmentView.test.js
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: MIT
 
 import React from 'react';
-import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { MemoryRouter, Route, Routes, Outlet } from 'react-router-dom';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen, waitFor, within, cleanup } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
@@ -29,6 +29,26 @@ describe('Tests for CreateAssignmentView components', () => {
       <MemoryRouter initialEntries={[`/${courseId}/create-attainment/${instanceId}`]}>
         <Routes>
           <Route path='/:courseId/create-attainment/:instanceId' element={<CreateAssignmentView/>}/>
+        </Routes>
+      </MemoryRouter>
+    );
+  };
+
+  const mockContext = {
+    addedAttainments: [],
+    setAddedAttainments: jest.fn(),
+    attainmentIncrementId: 0,
+    setIncrementId: jest.fn(),
+  };
+
+  const renderTemporaryCreateAssignmentView = async () => {
+
+    return render(
+      <MemoryRouter initialEntries={[`/${courseId}/create-temporary-attainment/${instanceId}`]}>
+        <Routes>
+          <Route element={<Outlet context={mockContext}/>}>
+            <Route path='/:courseId/create-temporary-attainment/:sisuInstanceId' element={<CreateAssignmentView/>}/>
+          </Route>
         </Routes>
       </MemoryRouter>
     );
@@ -61,7 +81,7 @@ describe('Tests for CreateAssignmentView components', () => {
     expect(expiryField).toBeInTheDocument();
     expect(creationButton).toBeInTheDocument();
     expect(confirmButton).toBeInTheDocument();
-  }),
+  });
 
   test('CreateAssignmentView should allow a teacher to create an attainment with a ready category', async () => {
 
@@ -151,26 +171,99 @@ describe('Tests for CreateAssignmentView components', () => {
     renderCreateAssignmentView();
 
     const creationButton = screen.getByText('Create Sub-Attainments');
-      expect(creationButton).toBeInTheDocument();
+    expect(creationButton).toBeInTheDocument();
 
-      // Create one sub-attainment
-      userEvent.click(creationButton);
+    // Create one sub-attainment
+    userEvent.click(creationButton);
 
-      const numberField = screen.getByLabelText('Number of sub-attainments');
-      expect(numberField).toBeInTheDocument();
+    const numberField = screen.getByLabelText('Number of sub-attainments');
+    expect(numberField).toBeInTheDocument();
 
-      const confirmButtons = await screen.findAllByText('Confirm');
-      const numConfirmButton = confirmButtons[1]; // the second one aka the one in the dialog
+    const confirmButtons = await screen.findAllByText('Confirm');
+    const numConfirmButton = confirmButtons[1]; // the second one aka the one in the dialog
 
-      // the default number of sub-attainments in the Dialog element is 1 so this call creates one sub-attainment
-      userEvent.click(numConfirmButton);
+    // the default number of sub-attainments in the Dialog element is 1 so this call creates one sub-attainment
+    userEvent.click(numConfirmButton);
 
-      // Check that there is one sub-attainment so one 'Delete'-button
-      const deleteButtons = await screen.findAllByText('Delete');
-      const addButton = screen.getByText('Add Sub-Attainments');
+    // Check that there is one sub-attainment so one 'Delete'-button
+    const deleteButtons = await screen.findAllByText('Delete');
+    const addButton = screen.getByText('Add Sub-Attainments');
 
-      expect(deleteButtons).toHaveLength(1);
-      expect(addButton).toBeInTheDocument();
+    expect(deleteButtons).toHaveLength(1);
+    expect(addButton).toBeInTheDocument();
+  });
+
+  test('CreateAssignmentView should render all of the appropriate components during instance creation', async () => {
+
+    renderTemporaryCreateAssignmentView();
+
+    const headingElement = screen.getByText('Create Study Attainment');
+    const selectLabel = 'Name';
+    const categoryField = await screen.findByLabelText(selectLabel);
+    const dateField = screen.getByLabelText('Date');
+    const expiryField = screen.getByLabelText('Expiry Date');
+    const creationButton = screen.getByText('Create Sub-Attainments');
+    const confirmButton = screen.getByText('Confirm');
+
+    userEvent.click(categoryField);
+    const optionsPopup = await screen.findByRole('listbox', { name: selectLabel });
+    userEvent.click(within(optionsPopup).getByText('Other'));
+
+    expect(await screen.findByText('Other')).toBeInTheDocument();
+
+    const nameField = screen.getByLabelText('New Name');
+
+    expect(headingElement).toBeInTheDocument();
+    expect(categoryField).toBeInTheDocument();
+    expect(nameField).toBeInTheDocument();
+    expect(dateField).toBeInTheDocument();
+    expect(expiryField).toBeInTheDocument();
+    expect(creationButton).toBeInTheDocument();
+    expect(confirmButton).toBeInTheDocument();
+  });
+
+  test('CreateAssignmentView should allow to add an attainment to context during instance creation', async () => {
+
+    const mockName = mockAttainment.name;
+    
+    // Mock request from client
+    const request = {
+      name: mockName,
+      date: mockDate.split('T')[0],
+      expiryDate: mockExpiryDate.split('T')[0],
+      subAttainments: [],
+      affectCalculation: false,
+      category: mockName,
+      formulaAttributes: [],
+    };
+
+    renderTemporaryCreateAssignmentView();
+
+    await waitFor( async () => {
+
+      const selectLabel = 'Name';
+      const categoryField = await screen.findByLabelText(selectLabel);
+      const dateField = screen.getByLabelText('Date');
+      const expiryField = screen.getByLabelText('Expiry Date');
+      const confirmButton = screen.getByText('Confirm');
+
+      userEvent.click(categoryField);
+      const optionsPopup = await screen.findByRole('listbox', { name: selectLabel });
+      userEvent.click(within(optionsPopup).getByText(mockName));
+
+      expect(await screen.findByText(mockName)).toBeInTheDocument();
+
+      userEvent.type(dateField, mockDate);
+      userEvent.type(expiryField, mockExpiryDate);
+      userEvent.click(confirmButton);
+
+      const temporaryId = mockContext.attainmentIncrementId;
+      const [updatedAttainments, newTemporaryId] = assignmentServices.createTemporaryAttainment(mockContext.addedAttainments, request, temporaryId);
+
+      expect(mockContext.setAddedAttainments).toHaveBeenCalledWith(updatedAttainments);
+      expect(mockContext.setIncrementId).toHaveBeenCalledWith(newTemporaryId);
+
+    });
   });
 
 });
--- client/src/tests/EditAssignmentView.test.js
+++ client/src/tests/EditAssignmentView.test.js
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: MIT
 
 import React from 'react';
-import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import { MemoryRouter, Routes, Route, Outlet } from 'react-router-dom';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen, waitFor, cleanup } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
@@ -17,6 +17,12 @@ const attainmentId = 2;  // Project
 
 const mockDate = '2023-09-30T00:00:00.000Z';  // again had some problems with dates,
 const mockExpiryDate = '2024-09-13T00:00:00.000Z';  // so I hardcoded them here
+
+const getMockAttainment = () => {
+  return JSON.parse(JSON.stringify(
+    [mockAttainmentsClient.find((attainment) => attainment.id === attainmentId)]
+  ));
+};
 
 assignmentServices.addAttainment = jest.fn();
 assignmentServices.editAttainment = jest.fn();
@@ -34,6 +40,29 @@ describe('Tests for EditAssignmentView components', () => {
     );
   };
 
+  let mockAttainment = getMockAttainment()[0];  // object
+  mockAttainment.temporaryId = mockAttainment.id;
+
+  const mockContext = {
+    addedAttainments: [mockAttainment],
+    setAddedAttainments: jest.fn(),
+    attainmentIncrementId: 0,
+    setIncrementId: jest.fn(),
+  };
+
+  const renderTemporaryEditAssignmentView = async () => {
+
+    return render(
+      <MemoryRouter initialEntries={[`/${courseId}/edit-temporary-attainment/${instanceId}/` + attainmentId]}>
+        <Routes>
+          <Route element={<Outlet context={mockContext}/>}>
+            <Route path='/:courseId/edit-temporary-attainment/:sisuInstanceId/:attainmentId' element={<EditAssignmentView/>}/>
+          </Route>
+        </Routes>
+      </MemoryRouter>
+    );
+  };
+
   test('EditAssignmentView should render the appropriate amount of components', async () => {
 
     renderEditAssignmentView();
@@ -43,7 +72,7 @@ describe('Tests for EditAssignmentView components', () => {
       const categoryField = await screen.findAllByLabelText('Name');
       const confirmButton = screen.getByText('Confirm');
 
-      let mockAttainment = [mockAttainmentsClient.find((attainment) => attainment.id === attainmentId)];
+      let mockAttainment = getMockAttainment();  // array with one object
       const numOfAttainments = assignmentServices.getNumOfAttainments(mockAttainment);
 
       expect(headingElement).toBeInTheDocument();
@@ -54,15 +83,12 @@ describe('Tests for EditAssignmentView components', () => {
 
   test('EditAssignmentView should only edit attainments if new ones are not created', async () => {
 
-    let mockAttainment = [mockAttainmentsClient.find((attainment) => attainment.id === attainmentId)][0];
-    mockAttainment.date = mockDate;
-    mockAttainment.expiryDate = mockExpiryDate;
-
     // Mock request from client,
     // TODO: needs to be modified to match the final format used by the server.
     // Now the format is different since data isn't gotten from the server yet.
-    const request = {};
-    Object.assign(request, mockAttainment)
+    let mockAttainment = getMockAttainment()[0];  // object
+    mockAttainment.date = mockDate;
+    mockAttainment.expiryDate = mockExpiryDate;
 
     renderEditAssignmentView();
 
@@ -86,27 +112,92 @@ describe('Tests for EditAssignmentView components', () => {
   test('EditAssignmentView should edit and add attainments if also new attainments are created', async () => {
 
     const newAttainment = {
-      name: 'New attainment',
-      date: mockDate,
+      name: '',
+      date: null,
       expiryDate: mockExpiryDate,
-      category: 'Other',
+      category: '',
+      parentId: attainmentId,
       subAttainments: [],
       affectCalculation: false,
       formulaAttributes: [],
     }
 
-    let mockAttainment = [mockAttainmentsClient.find((attainment) => attainment.id === attainmentId)][0];
+    // Mock request from client,
+    // TODO: needs to be modified to match the final format used by the server.
+    // Now the format is different since data isn't gotten from the server yet.
+    let mockAttainment = getMockAttainment()[0];  // object
     mockAttainment.date = mockDate;
     mockAttainment.expiryDate = mockExpiryDate;
     mockAttainment.subAttainments = [newAttainment];
 
-    // Mock request from client,
-    // TODO: needs to be modified to match the final format used by the server.
-    // Now the format is different since data isn't gotten from the server yet.
-    const request = {};
-    Object.assign(request, mockAttainment)
+    console.log(mockAttainment);
 
     renderEditAssignmentView();
+
+    // Set the top attainment's dates
+    const dateField = screen.getByLabelText('Date');
+    const expiryField = screen.getByLabelText('Expiry Date');
+
+    userEvent.type(dateField, mockDate);
+    userEvent.type(expiryField, mockExpiryDate);
+
+    // Create one sub-attainment:
+    const creationButton = screen.getByText('Create Sub-Attainments');
+    expect(creationButton).toBeInTheDocument();
+
+    userEvent.click(creationButton);
+
+    const numberField = screen.getByLabelText('Number of sub-attainments');
+    expect(numberField).toBeInTheDocument();
+
+    const confirmButtons = await screen.findAllByText('Confirm');
+    const numConfirmButton = confirmButtons[1]; // the second one aka the one in the dialog
+
+    // the default number of sub-attainments in the Dialog element is 1 so this call creates one sub-attainment
+    userEvent.click(numConfirmButton);
+
+    // Check that there is one sub-attainment so one 'Delete'-button
+    const deleteButtons = await screen.findAllByText('Delete');
+    const addButton = screen.getByText('Add Sub-Attainments');
+
+    expect(deleteButtons).toHaveLength(1);
+    expect(addButton).toBeInTheDocument();
+
+    // Edit the original attainment and add one sub attainment to it
+    userEvent.click(confirmButtons[0]);
+
+    expect(assignmentServices.editAttainment).toHaveBeenCalledWith(String(courseId), String(instanceId), mockAttainment);
+    expect(assignmentServices.addAttainment).toHaveBeenCalledWith(String(courseId), String(instanceId), newAttainment);
+
+  });
+
+  test('EditAssignmentView should render the appropriate amount of components during instance creation', async () => {
+
+    renderTemporaryEditAssignmentView();
+
+    await waitFor(async () => {
+      const headingElement = screen.getByText('Edit Study Attainment');
+      const categoryField = await screen.findAllByLabelText('Name');
+      const confirmButton = screen.getByText('Confirm');
+
+      let mockAttainment = getMockAttainment();  // array with one object
+      const numOfAttainments = assignmentServices.getNumOfAttainments(mockAttainment);
+
+      expect(headingElement).toBeInTheDocument();
+      expect(categoryField).toHaveLength(numOfAttainments);
+      expect(confirmButton).toBeInTheDocument();
+    });
+
+  });
+
+  test('EditAssignmentView should update the context during instance creation', async () => {
+
+    let mockAttainment = getMockAttainment()[0];  // object
+    mockAttainment.temporaryId = mockAttainment.id;
+    mockAttainment.date = mockAttainment.date.split('T')[0];
+    mockAttainment.expiryDate = mockAttainment.expiryDate.split('T')[0];
+
+    renderTemporaryEditAssignmentView();
 
     await waitFor(async () => {
 
@@ -119,8 +210,9 @@ describe('Tests for EditAssignmentView components', () => {
       const confirmButton = screen.getByText('Confirm');
       userEvent.click(confirmButton);
 
-      expect(assignmentServices.editAttainment).toHaveBeenCalledWith(String(courseId), String(instanceId), mockAttainment);
-      expect(assignmentServices.addAttainment).toHaveBeenCalledWith(String(courseId), String(instanceId), newAttainment);
+      const updatedAttainments = assignmentServices.updateTemporaryAttainment(mockContext.addedAttainments, mockAttainment);
+
+      expect(mockContext.setAddedAttainments).toHaveBeenCalledWith(updatedAttainments);
 
     });
 
