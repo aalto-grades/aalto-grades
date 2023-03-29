5a6edb74dd8bb8a0876784aa7d2cab1ec79e5d29
jj-stigell <63514007+jj-stigell@users.noreply.github.com>
2023-03-29 18:29:12 +0900

    refactor tests

--- server/test/controllers/grades.test.ts
+++ server/test/controllers/grades.test.ts
@@ -12,15 +12,22 @@ import { HttpCode } from '../../src/types/httpCode';
 const request: supertest.SuperTest<supertest.Test> = supertest(app);
 const badId: number = 1000000;
 const badInput: string = 'notValid';
+let res: supertest.Response;
 
-describe('Test POST /v1/courses/:courseId/instances/:instanceId/grades/csv', () => {
+function checkErrorRes(errorMessage: string, errorCode: HttpCode): void {
+  expect(res.body.success).toBe(false);
+  expect(res.body.errors).toContain(errorMessage);
+  expect(res.body.data).not.toBeDefined();
+  expect(res.statusCode).toBe(errorCode);
+}
+
+describe('YYYY Test POST /v1/courses/:courseId/instances/:instanceId/grades/csv', () => {
 
   it('should process CSV succesfully when course and course instance exist', async () => {
     const csvData: fs.ReadStream = fs.createReadStream(
       path.resolve(__dirname, '../mockData/csv/grades.csv'), 'utf8'
     );
-
-    const res: supertest.Response = await request
+    res = await request
       .post('/v1/courses/1/instances/1/grades/csv')
       .attach('csv_data', csvData, { contentType: 'text/csv'});
 
@@ -35,34 +42,25 @@ describe('Test POST /v1/courses/:courseId/instances/:instanceId/grades/csv', () 
       const invalidCsvData: fs.ReadStream = fs.createReadStream(
         path.resolve(__dirname, '../mockData/csv/grades_invalid_row.csv'), 'utf8'
       );
-
-      const res: supertest.Response = await request
+      res = await request
         .post('/v1/courses/1/instances/1/grades/csv')
         .attach('csv_data', invalidCsvData, { contentType: 'text/csv'});
 
-      expect(res.body.success).toBe(false);
-      expect(res.body.errors).toBeDefined();
-      expect(res.body.errors).toContain('Invalid Record Length: expect 6, got 5 on line 4');
-      expect(res.body.data).not.toBeDefined();
-      expect(res.statusCode).toBe(HttpCode.BadRequest);
+      checkErrorRes('Invalid Record Length: expect 6, got 5 on line 4', HttpCode.BadRequest);
     });
 
   it('should respond with 400 bad request, if the CSV file field name not "csv_data"', async () => {
     const csvData: fs.ReadStream = fs.createReadStream(
       path.resolve(__dirname, '../mockData/csv/grades.csv'), 'utf8'
     );
-
-    const res: supertest.Response = await request
+    res = await request
       .post('/v1/courses/1/instances/1/grades/csv')
       .attach(badInput, csvData, { contentType: 'text/csv'});
 
-    expect(res.body.success).toBe(false);
-    expect(res.body.errors).toBeDefined();
-    expect(res.body.errors).toContain(
-      'Unexpected field. To upload CSV file, set input field name as "csv_data"'
+    checkErrorRes(
+      'Unexpected field. To upload CSV file, set input field name as "csv_data"',
+      HttpCode.BadRequest
     );
-    expect(res.body.data).not.toBeDefined();
-    expect(res.statusCode).toBe(HttpCode.BadRequest);
   });
 
   it('should respond with 400 bad request, if the file content-type not text/csv',
@@ -70,18 +68,11 @@ describe('Test POST /v1/courses/:courseId/instances/:instanceId/grades/csv', () 
       const csvData: fs.ReadStream = fs.createReadStream(
         path.resolve(__dirname, '../mockData/csv/grades.csv'), 'utf8'
       );
-
-      const res: supertest.Response = await request
+      res = await request
         .post('/v1/courses/1/instances/1/grades/csv')
         .attach('csv_data', csvData, { contentType: 'application/json'});
 
-      expect(res.body.success).toBe(false);
-      expect(res.body.errors).toBeDefined();
-      expect(res.body.errors).toContain(
-        'incorrect file format, use the CSV format'
-      );
-      expect(res.body.data).not.toBeDefined();
-      expect(res.statusCode).toBe(HttpCode.BadRequest);
+      checkErrorRes('incorrect file format, use the CSV format', HttpCode.BadRequest);
     });
 
   it('should respond with 400 bad request, if the file extension incorrect (.txt)',
@@ -89,33 +80,23 @@ describe('Test POST /v1/courses/:courseId/instances/:instanceId/grades/csv', () 
       const txtFile: fs.ReadStream = fs.createReadStream(
         path.resolve(__dirname, '../mockData/csv/wrong_file_type.txt'), 'utf8'
       );
-
-      const res: supertest.Response = await request
+      res = await request
         .post('/v1/courses/1/instances/1/grades/csv')
         .attach('csv_data', txtFile, { contentType: 'text/csv'});
 
-      expect(res.body.success).toBe(false);
-      expect(res.body.errors).toBeDefined();
-      expect(res.body.errors).toContain(
-        'incorrect file format, use the CSV format'
-      );
-      expect(res.body.data).not.toBeDefined();
-      expect(res.statusCode).toBe(HttpCode.BadRequest);
+      checkErrorRes('incorrect file format, use the CSV format', HttpCode.BadRequest);
     });
 
   it('should respond with 400 bad request, if the CSV file not found in the request.',
     async () => {
-      const res: supertest.Response = await request
+      res = await request
         .post('/v1/courses/1/instances/1/grades/csv')
         .attach('csv_data', false, { contentType: 'text/csv'});
 
-      expect(res.body.success).toBe(false);
-      expect(res.body.errors).toBeDefined();
-      expect(res.body.errors).toContain(
-        'CSV file not found in the request. To upload CSV file, set input field name as "csv_data"'
+      checkErrorRes(
+        'CSV file not found in the request. To upload CSV file, set input field name as "csv_data"',
+        HttpCode.BadRequest
       );
-      expect(res.body.data).not.toBeDefined();
-      expect(res.statusCode).toBe(HttpCode.BadRequest);
     });
 
   it('should respond with 400 bad request, if validation fails (non-number course id)',
@@ -123,87 +104,64 @@ describe('Test POST /v1/courses/:courseId/instances/:instanceId/grades/csv', () 
       const csvData: fs.ReadStream = fs.createReadStream(
         path.resolve(__dirname, '../mockData/csv/grades.csv'), 'utf8'
       );
-
-      const res: supertest.Response = await request
+      res = await request
         .post(`/v1/courses/${badInput}/instances/1/grades/csv`)
         .attach('csv_data', csvData, { contentType: 'text/csv'});
 
-      expect(res.body.success).toBe(false);
-      expect(res.body.errors).toBeDefined();
-      expect(res.body.errors).toContain(
-        'id must be a `number` type, but the final value was: `NaN` (cast from the value `NaN`).'
+      checkErrorRes(
+        'id must be a `number` type, but the final value was: `NaN` (cast from the value `NaN`).',
+        HttpCode.BadRequest
       );
-      expect(res.body.data).not.toBeDefined();
-      expect(res.statusCode).toBe(HttpCode.BadRequest);
     });
-
 
   it('should respond with 400 bad request, if validation fails (non-number course instance id)',
     async () => {
       const csvData: fs.ReadStream = fs.createReadStream(
         path.resolve(__dirname, '../mockData/csv/grades.csv'), 'utf8'
       );
-
-      const res: supertest.Response = await request
+      res = await request
         .post(`/v1/courses/1/instances/${badInput}/grades/csv`)
         .attach('csv_data', csvData, { contentType: 'text/csv'});
 
-      expect(res.body.success).toBe(false);
-      expect(res.body.errors).toBeDefined();
-      expect(res.body.errors).toContain(
-        'id must be a `number` type, but the final value was: `NaN` (cast from the value `NaN`).'
+      checkErrorRes(
+        'id must be a `number` type, but the final value was: `NaN` (cast from the value `NaN`).',
+        HttpCode.BadRequest
       );
-      expect(res.body.data).not.toBeDefined();
-      expect(res.statusCode).toBe(HttpCode.BadRequest);
     });
 
   it('should respond with 404 not found, if course does not exist', async () => {
     const csvData: fs.ReadStream = fs.createReadStream(
       path.resolve(__dirname, '../mockData/csv/grades.csv'), 'utf8'
     );
-
-    const res: supertest.Response = await request
+    res = await request
       .post(`/v1/courses/${badId}/instances/1/grades/csv`)
       .attach('csv_data', csvData, { contentType: 'text/csv'});
 
-    expect(res.body.success).toBe(false);
-    expect(res.body.errors).toBeDefined();
-    expect(res.body.errors).toContain(`course with ID ${badId} not found`);
-    expect(res.body.data).not.toBeDefined();
-    expect(res.statusCode).toBe(HttpCode.NotFound);
+    checkErrorRes(`course with ID ${badId} not found`, HttpCode.NotFound);
   });
 
   it('should respond with 404 not found, if course instance does not exist', async () => {
     const csvData: fs.ReadStream = fs.createReadStream(
       path.resolve(__dirname, '../mockData/csv/grades.csv'), 'utf8'
     );
-
-    const res: supertest.Response = await request
+    res = await request
       .post(`/v1/courses/1/instances/${badId}/grades/csv`)
       .attach('csv_data', csvData, { contentType: 'text/csv'});
 
-    expect(res.body.success).toBe(false);
-    expect(res.body.errors).toBeDefined();
-    expect(res.body.errors).toContain(`course instance with ID ${badId} not found`);
-    expect(res.body.data).not.toBeDefined();
-    expect(res.statusCode).toBe(HttpCode.NotFound);
+    checkErrorRes(`course instance with ID ${badId} not found`, HttpCode.NotFound);
   });
 
   it('should respond with 409 conflict, if instance does not belong to the course', async () => {
     const csvData: fs.ReadStream = fs.createReadStream(
       path.resolve(__dirname, '../mockData/csv/grades.csv'), 'utf8'
     );
-
-    const res: supertest.Response = await request
+    res = await request
       .post('/v1/courses/1/instances/2/grades/csv')
       .attach('csv_data', csvData, { contentType: 'text/csv'});
 
-    expect(res.body.success).toBe(false);
-    expect(res.body.errors).toBeDefined();
-    expect(res.body.errors).toContain(
-      'course instance with ID 2 does not belong to the course with ID 1'
+    checkErrorRes(
+      'course instance with ID 2 does not belong to the course with ID 1',
+      HttpCode.Conflict
     );
-    expect(res.body.data).not.toBeDefined();
-    expect(res.statusCode).toBe(HttpCode.Conflict);
   });
 });
