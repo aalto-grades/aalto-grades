9f521cf918751cfaab470692bfd3cf06fe7fd4a2
no-comma <82644961+no-comma@users.noreply.github.com>
2023-04-16 12:33:40 +0300

    Add semicolon

--- server/src/controllers/grades.ts
+++ server/src/controllers/grades.ts
@@ -264,7 +264,7 @@ export async function calculateGrades(
         studentNumber: attainmentGrade.User.studentId,
         grade: attainmentGrade.grade,
         attainmentId: attainmentGrade.attainableId
-      }
+      };
     }
   );
 
