70b00dadede825048f752f0e560d8531dd3d2ce0
no-comma <82644961+no-comma@users.noreply.github.com>
2023-04-16 20:06:44 +0300

    Fix mock data

--- server/mockData/attainable.sql
+++ server/mockData/attainable.sql
@@ -2,7 +2,7 @@
 --
 -- SPDX-License-Identifier: MIT
 
-INSERT INTO public.attainable (course_id, course_instance_id, attainable_id, name, date, expiry_date, created_at, updated_at, formula_id, formula_params) VALUES
+INSERT INTO public.attainable (course_id, course_instance_id, attainable_id, name, date, expiry_date, created_at, updated_at, formula, parent_formula_params) VALUES
 (1, 1, null, 'test assignment 1.1', '2022-01-13', '2023-04-23', NOW(), NOW(), 'MANUAL', NULL),
 (2, 2, null, 'test assignment tree', '2022-05-08', '2023-04-23', NOW(), NOW(), 'MANUAL', NULL),
 (3, 3, null, 'test assignment 3.1', '2022-08-05', '2023-04-23', NOW(), NOW(), 'MANUAL', NULL),
