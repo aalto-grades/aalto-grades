db1b7e41c5fddb0b1649fb69141eb5532f7c33d4
no-comma <82644961+no-comma@users.noreply.github.com>
2023-04-20 14:34:02 +0300

    Write remaining tests

--- server/test/controllers/grades.test.ts
+++ server/test/controllers/grades.test.ts
@@ -6,7 +6,11 @@ import * as fs from 'fs';
 import path from 'path';
 import supertest from 'supertest';
 
+import Attainable from '../../src/database/models/attainable';
+import UserAttainmentGrade from '../../src/database/models/userAttainmentGrade';
+
 import { app } from '../../src/app';
+import { Formula } from '../../src/types/formulas';
 import { HttpCode } from '../../src/types/httpCode';
 
 const request: supertest.SuperTest<supertest.Test> = supertest(app);
@@ -167,32 +171,340 @@ describe('Test POST /v1/courses/:courseId/instances/:instanceId/grades/csv', () 
 });
 
 describe('Test POST /v1/courses/:courseId/instances/:instanceId/grades/calculate', () => {
-  it('should calculate one correct grade', async () => {
-    const res: supertest.Response = await request
-      .post('/v1/courses/5/instances/8/grades/calculate');
+  /*
+   * no-explicit-any is disabled in the following tests to mock Sequelize
+   * return values more easily.
+   */
 
+  function checkSuccessRes(res: supertest.Response, finalGrades: Array<object>): void {
     expect(res.body.errors).not.toBeDefined();
     expect(res.body.success).toBe(true);
     expect(res.statusCode).toBe(HttpCode.Ok);
     expect(res.body.data).toBeDefined();
-    expect(res.body.data.grades).toEqual([
-      {
-        studentNumber: '352772',
-        grade: 1.24,
-        status: 'pass'
-      }
-    ]);
+    expect(res.body.data.grades).toEqual(finalGrades);
+  }
+
+  it('should calculate one correct grade', async () => {
+    checkSuccessRes(
+      await request.post('/v1/courses/5/instances/8/grades/calculate'),
+      [
+        {
+          studentNumber: '352772',
+          grade: 1.24,
+          status: 'pass'
+        }
+      ]
+    );
   });
 
   it('should calculate multiple correct grades', async () => {
-    // TODO
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    jest.spyOn(Attainable, 'findAll').mockImplementation((): any => {
+      return [
+        {
+          id: 1,
+          parentId: null,
+          formula: Formula.WeightedAverage,
+          parentFormulaParams: null
+        },
+        {
+          id: 2,
+          parentId: 1,
+          formula: Formula.Manual,
+          parentFormulaParams: {
+            min: 0,
+            max: 5,
+            weight: 0.75
+          }
+        },
+        {
+          id: 3,
+          parentId: 1,
+          formula: Formula.Manual,
+          parentFormulaParams: {
+            min: 0,
+            max: 5,
+            weight: 0.25
+          }
+        },
+      ];
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    jest.spyOn(UserAttainmentGrade, 'findAll').mockImplementation((): any => {
+      return [
+        {
+          User: {
+            studentId: '111111'
+          },
+          grade: 1,
+          attainableId: 2,
+        },
+        {
+          User: {
+            studentId: '111111'
+          },
+          grade: 3,
+          attainableId: 3,
+        },
+        {
+          User: {
+            studentId: '222222'
+          },
+          grade: 5,
+          attainableId: 2,
+        },
+        {
+          User: {
+            studentId: '222222'
+          },
+          grade: 4,
+          attainableId: 3,
+        },
+        {
+          User: {
+            studentId: '333333'
+          },
+          grade: 4,
+          attainableId: 2,
+        },
+        {
+          User: {
+            studentId: '333333'
+          },
+          grade: 1,
+          attainableId: 3,
+        }
+      ];
+    });
+
+    checkSuccessRes(
+      await request.post('/v1/courses/1/instances/1/grades/calculate'),
+      [
+        {
+          studentNumber: '111111',
+          grade: 1.5,
+          status: 'pass'
+        },
+        {
+          studentNumber: '222222',
+          grade: 4.75,
+          status: 'pass'
+        },
+        {
+          studentNumber: '333333',
+          grade: 3.25,
+          status: 'pass'
+        }
+      ]
+    );
   });
 
   it('should calculate correct grades in higher depths', async () => {
-    // TODO
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    jest.spyOn(Attainable, 'findAll').mockImplementation((): any => {
+      return [
+        {
+          id: 1,
+          parentId: null,
+          formula: Formula.WeightedAverage,
+          parentFormulaParams: null
+        },
+        {
+          id: 2,
+          parentId: 1,
+          formula: Formula.Manual,
+          parentFormulaParams: {
+            min: 0,
+            max: 5,
+            weight: 0.4
+          }
+        },
+        {
+          id: 3,
+          parentId: 1,
+          formula: Formula.WeightedAverage,
+          parentFormulaParams: {
+            min: 0,
+            max: 5,
+            weight: 0.6
+          }
+        },
+        {
+          id: 4,
+          parentId: 3,
+          formula: Formula.Manual,
+          parentFormulaParams: {
+            min: 0,
+            max: 5,
+            weight: 0.1
+          }
+        },
+        {
+          id: 5,
+          parentId: 3,
+          formula: Formula.Manual,
+          parentFormulaParams: {
+            min: 0,
+            max: 5,
+            weight: 0.1
+          }
+        },
+        {
+          id: 6,
+          parentId: 3,
+          formula: Formula.WeightedAverage,
+          parentFormulaParams: {
+            min: 0,
+            max: 5,
+            weight: 0.8
+          }
+        },
+        {
+          id: 7,
+          parentId: 6,
+          formula: Formula.Manual,
+          parentFormulaParams: {
+            min: 0,
+            max: 5,
+            weight: 0.5
+          }
+        },
+        {
+          id: 8,
+          parentId: 6,
+          formula: Formula.Manual,
+          parentFormulaParams: {
+            min: 0,
+            max: 5,
+            weight: 0.5
+          }
+        }
+      ];
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    jest.spyOn(UserAttainmentGrade, 'findAll').mockImplementation((): any => {
+      return [
+        {
+          User: {
+            studentId: '123456'
+          },
+          grade: 3,
+          attainableId: 2,
+        },
+        {
+          User: {
+            studentId: '123456'
+          },
+          grade: 4,
+          attainableId: 4,
+        },
+        {
+          User: {
+            studentId: '123456'
+          },
+          grade: 4,
+          attainableId: 5,
+        },
+        {
+          User: {
+            studentId: '123456'
+          },
+          grade: 1,
+          attainableId: 7,
+        },
+        {
+          User: {
+            studentId: '123456'
+          },
+          grade: 5,
+          attainableId: 8,
+        }
+      ];
+    });
+
+    checkSuccessRes(
+      await request.post('/v1/courses/1/instances/1/grades/calculate'),
+      [
+        {
+          studentNumber: '123456',
+          grade: 3.12,
+          status: 'pass'
+        }
+      ]
+    );
   });
 
   it('should allow manually overriding a student\'s grade', async () => {
-    // TODO
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    jest.spyOn(Attainable, 'findAll').mockImplementation((): any => {
+      return [
+        {
+          id: 1,
+          parentId: null,
+          formula: Formula.WeightedAverage,
+          parentFormulaParams: null
+        },
+        {
+          id: 2,
+          parentId: 1,
+          formula: Formula.Manual,
+          parentFormulaParams: {
+            min: 0,
+            max: 5,
+            weight: 0.5
+          }
+        },
+        {
+          id: 3,
+          parentId: 1,
+          formula: Formula.WeightedAverage,
+          parentFormulaParams: {
+            min: 0,
+            max: 5,
+            weight: 0.5
+          }
+        }
+      ];
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    jest.spyOn(UserAttainmentGrade, 'findAll').mockImplementation((): any => {
+      return [
+        {
+          User: {
+            studentId: '654321'
+          },
+          grade: 5,
+          attainableId: 1,
+        },
+        {
+          User: {
+            studentId: '654321'
+          },
+          grade: 0,
+          attainableId: 2,
+        },
+        {
+          User: {
+            studentId: '654321'
+          },
+          grade: 0,
+          attainableId: 3,
+        }
+      ];
+    });
+
+    checkSuccessRes(
+      await request.post('/v1/courses/1/instances/1/grades/calculate'),
+      [
+        {
+          studentNumber: '654321',
+          grade: 5,
+          status: 'pass'
+        }
+      ]
+    );
   });
 });
