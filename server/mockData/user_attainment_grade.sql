57f7186321f7166ebb6d79274b40b384bf1bebac
Jere Stigell <jj.stigell@gmail.com>
2023-05-08 16:46:15 +0300

    add missing reuse

--- server/mockData/user_attainment_grade.sql
+++ server/mockData/user_attainment_grade.sql
@@ -1,3 +1,7 @@
+-- SPDX-FileCopyrightText: 2022 The Aalto Grades Developers
+--
+-- SPDX-License-Identifier: MIT
+
 INSERT INTO public.user_attainment_grade (user_id, attainable_id, grade, created_at, updated_at) VALUES
 (1, 14, 0.37, NOW(), NOW()),
 (1, 15, 0.5, NOW(), NOW());
