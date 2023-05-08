276f13305fbbecb9e9fd60aeaef079c45eaa78f0
Jere Stigell <jj.stigell@gmail.com>
2023-05-08 12:20:12 +0300

    REUSE

--- server/src/configs/env.ts
+++ server/src/configs/env.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 The Aalto Grades Developers
+//
+// SPDX-License-Identifier: MIT
+
 // Config dotenv so environment variables are accessible from .env file.
 import * as dotenv from 'dotenv';
 dotenv.config();
