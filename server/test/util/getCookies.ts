665539e6217950cee2a71193241a0522dced7fb8
Jere Stigell <jj.stigell@gmail.com>
2023-06-05 14:23:46 +0300

    return both user and admin cookies

--- server/test/util/getCookies.ts
+++ server/test/util/getCookies.ts
@@ -5,29 +5,57 @@
 import supertest from 'supertest';
 
 import { app } from '../../src/app';
+import { UserRole } from '../../src/types/general';
 
 const request: supertest.SuperTest<supertest.Test> = supertest(app);
 
-export async function getCookies(): Promise<Array<string>> {
-  const email: string = `${new Date().getTime()}@aalto.fi`;
+export interface Cookies {
+  adminCookie: Array<string>
+  userCookie: Array<string>
+}
+
+export async function getCookies(): Promise<Cookies> {
+  const adminEmail: string = `admin${new Date().getTime()}@aalto.fi`;
+  const userEmail: string = `user${new Date().getTime()}@aalto.fi`;
 
   await request.post('/v1/auth/signup')
     .set('Accept', 'application/json')
     .send({
-      email,
+      email: adminEmail,
       name: 'aalto tester',
       password: 'grades',
       studentID: new Date().getTime(),
-      role: 'SYSADMIN'
+      role: UserRole.Admin
     });
 
-  const res: supertest.Response = await request
+  await request.post('/v1/auth/signup')
+    .set('Accept', 'application/json')
+    .send({
+      email: userEmail,
+      name: 'aalto tester',
+      password: 'grades',
+      studentID: new Date().getTime(),
+      role: UserRole.User
+    });
+
+  const adminRes: supertest.Response = await request
     .post('/v1/auth/login')
     .set('Accept', 'application/json')
     .send({
-      email,
+      email: adminEmail,
       password: 'grades'
     });
 
-  return res.headers['set-cookie'];
+  const userRes: supertest.Response = await request
+    .post('/v1/auth/login')
+    .set('Accept', 'application/json')
+    .send({
+      email: userEmail,
+      password: 'grades'
+    });
+
+  return {
+    adminCookie: adminRes.headers['set-cookie'],
+    userCookie: userRes.headers['set-cookie']
+  };
 }
