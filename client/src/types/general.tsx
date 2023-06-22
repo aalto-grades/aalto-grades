4fd62977b5fa63b630d7c063bbee58edef47eb0b
no-comma <82644961+no-comma@users.noreply.github.com>
2023-06-22 11:24:59 +0000

    Assessment model services (#308)

--- client/src/types/general.tsx
+++ client/src/types/general.tsx
@@ -2,11 +2,17 @@
 //
 // SPDX-License-Identifier: MIT
 
+import { AxiosResponse } from 'axios';
+
 export interface ApiResponse<T> {
   success: boolean,
   data?: T,
   errors?: Array<string>
 }
+
+export type FullResponse<T> = AxiosResponse<ApiResponse<T>, unknown>;
+
+export type Numeric = number | string;
 
 export interface Message {
   msg: string,
