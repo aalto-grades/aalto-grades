600a9f3da98e52a74ab7b8ac68ef3c706e552d39
no-comma <82644961+no-comma@users.noreply.github.com>
2023-06-22 08:41:33 +0000

    Get all assessment models of course (#305)

--- server/src/routes/assessmentModel.ts
+++ server/src/routes/assessmentModel.ts
@@ -5,7 +5,9 @@
 import { Router } from 'express';
 import passport from 'passport';
 
-import { getAssessmentModel } from '../controllers/assessmentModel';
+import {
+  getAllAssessmentModels, getAssessmentModel
+} from '../controllers/assessmentModel';
 import { controllerDispatcher } from '../middleware/errorHandler';
 
 export const router: Router = Router();
@@ -45,8 +47,8 @@ export const router: Router = Router();
  *     responses:
  *       200:
  *         description: >
- *           A course instance with the given ID was found and information
- *           about it is returned in the CourseInstanceData format.
+ *           An assessment model with the given ID was found and information
+ *           about it is returned in the AssessmentModelData format.
  *         content:
  *           application/json:
  *             schema:
@@ -57,7 +59,7 @@ export const router: Router = Router();
  *                 data:
  *                   type: object
  *                   properties:
- *                     courseInstance:
+ *                     assessmentModel:
  *                       $ref: '#/definitions/AssessmentModelData'
  *       400:
  *         description: >
@@ -92,4 +94,59 @@ router.get(
   '/v1/courses/:courseId/assessment-models/:assessmentModelId',
   passport.authenticate('jwt', { session: false }),
   controllerDispatcher(getAssessmentModel)
+);
+
+/**
+ * @swagger
+ * /v1/courses/{courseId}/assessment-models:
+ *   get:
+ *     tags: [Assessment Model]
+ *     description: Get information about all the assessment models of a course.
+ *     parameters:
+ *       - $ref: '#/components/parameters/courseId'
+ *       - $ref: '#/components/parameters/assessmentModelId'
+ *     responses:
+ *       200:
+ *         description: >
+ *           A course with the given ID was found and information about all of
+ *           its assessment models, if any, are returned in the
+ *           AssessmentModelData format.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   $ref: '#/definitions/Success'
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     assessmentModels:
+ *                       type: array
+ *                       items:
+ *                         $ref: '#/definitions/AssessmentModelData'
+ *       400:
+ *         description: >
+ *           A validation error has occurred in the URL, the given course ID is
+ *           not a positive integer.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/definitions/Failure'
+ *       401:
+ *         $ref: '#/components/responses/AuthenticationError'
+ *       404:
+ *         description: >
+ *           A course with the given course ID was not found.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/definitions/Failure'
+ *     security:
+ *       - cookieAuth: []
+ */
+router.get(
+  '/v1/courses/:courseId/assessment-models',
+  passport.authenticate('jwt', { session: false }),
+  controllerDispatcher(getAllAssessmentModels)
 );
