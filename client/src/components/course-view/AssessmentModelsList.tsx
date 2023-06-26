ac3f67bbc1470ea28a8f8a969e2542965ef0398e
no-comma <82644961+no-comma@users.noreply.github.com>
2023-06-26 14:50:59 +0300

    Start updating course view

---
+++ client/src/components/course-view/AssessmentModelsList.tsx
@@ -0,0 +1,41 @@
+// SPDX-FileCopyrightText: 2023 The Aalto Grades Developers
+//
+// SPDX-License-Identifier: MIT
+
+import { JSX } from 'react';
+import PropTypes from 'prop-types';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import { AssessmentModelData } from 'aalto-grades-common/types';
+
+function AssessmentModelsList(props: {
+  data: Array<AssessmentModelData>,
+  current: number,
+  onClick: (assessmentModel: AssessmentModelData) => void
+}): JSX.Element {
+  return (
+    <List>
+      {
+        props.data.map((assessmentModel: AssessmentModelData) => {
+          return (
+            <ListItemButton
+              key={assessmentModel.id}
+              selected={props.current === assessmentModel.id}
+              onClick={(): void => props.onClick(assessmentModel)}
+            >
+              {assessmentModel.name}
+            </ListItemButton>
+          );
+        })
+      }
+    </List>
+  );
+}
+
+AssessmentModelsList.propTypes = {
+  data: PropTypes.array,
+  current: PropTypes.number,
+  onClick: PropTypes.func
+};
+
+export default AssessmentModelsList;
---
+++ client/src/components/course-view/CourseDetails.tsx
@@ -0,0 +1,59 @@
+// SPDX-FileCopyrightText: 2022 The Aalto Grades Developers
+//
+// SPDX-License-Identifier: MIT
+
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import LightLabelBoldValue from '../typography/LightLabelBoldValue';
+import { CourseData } from 'aalto-grades-common/types';
+
+function CourseDetails(params: {
+  course: CourseData
+}) {
+  return (
+    <Box sx={{ display: 'inline-block', pt: 1.5 }}>
+      <Box sx={{
+        display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between',
+        alignItems: 'center', columnGap: 4, pb: 1
+      }}>
+        <Typography variant='h3' align='left' sx={{ ml: 1.5 }} >
+          Course Details
+        </Typography>
+      </Box>
+      <Box textAlign='left' borderRadius={1} sx={{
+        bgcolor: 'secondary.light', p: 1.5, mt: 1, minWidth: '318px'
+      }}>
+        <LightLabelBoldValue
+          label='Credits'
+          value={`${params.course.minCredits}-${params.course.maxCredits}`}
+        />
+        <LightLabelBoldValue
+          label='Organizer'
+          value={params.course.department.en}
+        />
+        <LightLabelBoldValue
+          label='Educational Institution'
+          // REPLACE SOME DAY? currently this info can't be fetched from database
+          value='Aalto University'
+        />
+      </Box>
+      <Box sx={{ m: 1.5 }}>
+        <Typography variant='h3' align='left' sx={{ pt: 1.5, pb: 1 }}>
+          Teachers in Charge
+        </Typography>
+        {/*
+          params.course.teachersInCharge.map((teacher) => <Typography align='left' key={teacher} >
+            {teacher}
+          </Typography>)
+        */}
+      </Box>
+    </Box>
+  );
+}
+
+CourseDetails.propTypes = {
+  course: PropTypes.object,
+};
+
+export default CourseDetails;
