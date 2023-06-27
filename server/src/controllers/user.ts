639527a6955fa627ee903db612fa914dbcd156f0
no-comma <82644961+no-comma@users.noreply.github.com>
2023-06-27 09:45:40 +0300

    Update courses of user API endpoint

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -9,7 +9,7 @@ import CourseInstance from '../database/models/courseInstance';
 import CourseTranslation from '../database/models/courseTranslation';
 import User from '../database/models/user';
 
-import { CourseData, CoursesOfUser } from 'aalto-grades-common/types';
+import { CourseData } from 'aalto-grades-common/types';
 import { idSchema } from '../types/general';
 import { HttpCode } from '../types/httpCode';
 import { parseCourseFull } from './utils/course';
@@ -17,7 +17,7 @@ import { findUserById } from './utils/user';
 import { CourseFull } from '../types/model';
 
 export async function getCoursesOfUser(req: Request, res: Response): Promise<void> {
-  const coursesOfUser: CoursesOfUser = { current: [], previous: [] };
+  const courses: Array<CourseData> = [];
   const userId: number = Number(req.params.userId);
   await idSchema.validate({ id: userId });
 
@@ -44,20 +44,16 @@ export async function getCoursesOfUser(req: Request, res: Response): Promise<voi
       ]
     }) as Array<CourseFull>;
 
-  interface CourseFullWithInstances extends CourseFull {
-    CourseInstances: Array<CourseInstance>;
-    CourseTranslations: Array<CourseTranslation>
-  }
-
-  const instanceRoleCourses: Array<CourseFullWithInstances> =
+  const instanceRoleCourses: Array<CourseFull> =
     await Course.findAll({
       include: [
         {
           model: CourseInstance,
-          attributes: ['endDate'],
+          attributes: [],
           include: [
             {
               model: User,
+              attributes: [],
               where: {
                 id: userId
               }
@@ -70,37 +66,24 @@ export async function getCoursesOfUser(req: Request, res: Response): Promise<voi
         {
           model: User
         }
-      ],
-      order: [[CourseInstance, 'endDate', 'DESC']]
-    }) as Array<CourseFullWithInstances>;
+      ]
+    }) as Array<CourseFull>;
 
   for (const course of inChargeCourses) {
-    coursesOfUser.current.push(parseCourseFull(course));
+    courses.push(parseCourseFull(course));
   }
 
-  // Construct CourseData objects and determine whether the course is current or previous.
-  const currentDate: Date = new Date(Date.now());
   for (const course of instanceRoleCourses) {
-    // Don't include courses that have already been included as courses the
-    // user is in charge of
-    if (coursesOfUser.current.find((course: CourseData) => course.id === course.id))
+    if (courses.find((added: CourseData) => added.id === course.id))
       continue;
 
-    const courseData: CourseData = parseCourseFull(course);
-
-    const latestEndDate: Date = new Date(String(course.CourseInstances[0].endDate));
-
-    if (currentDate <= latestEndDate) {
-      coursesOfUser.current.push(courseData);
-    } else {
-      coursesOfUser.previous.push(courseData);
-    }
+    courses.push(parseCourseFull(course));
   }
 
   res.status(HttpCode.Ok).send({
     success: true,
     data: {
-      courses: coursesOfUser
+      courses: courses
     }
   });
 }
