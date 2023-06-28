0dbd5d53960985b030201927b4fd904d85ad2c7e
Jere Stigell <jj.stigell@gmail.com>
2023-06-28 11:15:32 +0300

    update code snippet

--- server/src/formulas/weightedAverage.ts
+++ server/src/formulas/weightedAverage.ts
@@ -40,17 +40,33 @@ async function calculateWeightedAverage(
 }
 
 const codeSnippet: string =
-`const weightedAverage = (nums, weights) => {
-  const [sum, weightSum] = weights.reduce(
-    (acc, w, i) => {
-      acc[0] = acc[0] + nums[i] * w;
-      acc[1] = acc[1] + w;
-      return acc;
-    },
-    [0, 0]
-  );
-  return sum / weightSum;
-};`;
+`interface WeightedAverageParams {
+  min: number;
+  max: number;
+  weight: number;
+}
+
+async function calculateWeightedAverage(
+  inputs: Array<CalculationInput>
+): Promise<CalculationResult> {
+  let grade: number = 0;
+  let status: Status = Status.Pass;
+
+  for (const input of inputs) {
+    const subResult: CalculationResult = input.subResult;
+    const params: WeightedAverageParams = input.params as WeightedAverageParams;
+
+    if (subResult.status !== Status.Pass)
+      status = Status.Fail;
+
+    grade += subResult.grade * params.weight;
+  }
+
+  return {
+    grade: grade,
+    status: status,
+  };
+}`;
 
 const name: string = 'Weighted average';
 
