40b64c27a43f347e9dc66e3ccb190cd0c94d19ba
no-comma <82644961+no-comma@users.noreply.github.com>
2023-07-03 11:43:13 +0300

    Start fixing tests

--- client/src/tests/mock-data/mockAttainments.tsx
+++ client/src/tests/mock-data/mockAttainments.tsx
@@ -2,35 +2,29 @@
 //
 // SPDX-License-Identifier: MIT
 
+import { AttainmentData } from 'aalto-grades-common/types';
+
 // These mock attainments are temporary and they will be modified later
 // They have been kept in order to not break any tests or content currently in main
 
-const mockAttainments = [
+const mockAttainments: Array<AttainmentData> = [
   {
     id: 1,
-    type: 'Exercises',
     name: 'Exercises',
-    description: '10 mandatory exercises',
-    points: 30,
-    weight: 0.2,
-    expiryDate: new Date(2024, 8, 14)
+    tag: 'exercise',
+    daysValid: 100
   },
   {
-    id:2,
-    type: 'Projects',
+    id: 2,
     name: 'Projects',
-    description: '1 mandatory project',
-    points: 30,
-    weight: 0.20,
-    expiryDate: new Date(2024, 8, 14) },
+    tag: 'project',
+    daysValid: 1
+  },
   {
     id: 3,
-    type: 'Exams',
     name: 'Exams',
-    description: '1 mandatory exam',
-    points: 40,
-    weight: 0.55,
-    expiryDate: new Date(2024, 8, 14)
+    tag: 'exam',
+    daysValid: 365
   }
 ];
 
--- client/src/tests/mock-data/mockAttainmentsClient.tsx
+++ client/src/tests/mock-data/mockAttainmentsClient.tsx
@@ -2,105 +2,58 @@
 //
 // SPDX-License-Identifier: MIT
 
-const mockAttainmentsClient = {
+import { AttainmentData } from 'aalto-grades-common/types';
+
+const mockAttainmentsClient: AttainmentData = {
+  id: 10000,
+  name: 'Root',
+  tag: 'root',
+  daysValid: 0,
   subAttainments: [
     {
       id: 1,
       name: 'Exercises',
       tag: 'test exercises',
-      category: 'Other',
-      date: new Date(2023, 9, 1),
-      expiryDate: new Date(2024, 8, 14),
-      formulaId: 1,  // formulaId is specified for attainments that have sub-attinments
+      daysValid: 100,
       subAttainments: [
         {
           id: 11,
           name: '4 mandatory exercises',
           tag: 'test tag 1',
-          category: 'Other',
-          date: new Date(2023, 9, 1),
-          expiryDate: new Date(2024, 8, 14),
-          formulaId: 1,
-          affectCalculation: true,
-          formulaAttributes: {
-            maxPoints: '',
-            minRequiredPoints: '',
-            weight: ''
-          },
+          daysValid: 100,
           subAttainments: [
             {
               id: 111,
               name: 'Exercise 1',
               tag: 'test tag 2',
-              category: 'Other',
-              date: new Date(2023, 9, 1),
-              expiryDate: new Date(2024, 8, 14),
-              affectCalculation: true,
-              formulaAttributes: {
-                maxPoints: '',
-                minRequiredPoints: '',
-                weight: ''
-              },
+              daysValid: 100,
               subAttainments: [],
             },
             {
               id: 112,
               name: 'Exercise 2',
               tag: 'test tag 3',
-              category: 'Other',
-              date: new Date(2023, 9, 1),
-              expiryDate: new Date(2024, 8, 14),
-              affectCalculation: true,
-              formulaAttributes: {
-                maxPoints: '',
-                minRequiredPoints: '',
-                weight: ''
-              },
+              daysValid: 100,
               subAttainments: [],
             },
             {
               id: 113,
               name: 'Exercise 3',
               tag: 'test tag 4',
-              category: 'Other',
-              date: new Date(2023, 9, 1),
-              expiryDate: new Date(2024, 8, 14),
-              formulaId: 1,
-              affectCalculation: true,
-              formulaAttributes: {
-                maxPoints: '',
-                minRequiredPoints: '',
-                weight: ''
-              },
+              daysValid: 100,
               subAttainments: [
                 {
                   id: 1131,
                   name: 'Exercise 3.1',
                   tag: 'test tag 5',
-                  category: 'Other',
-                  date: new Date(2023, 9, 1),
-                  expiryDate: new Date(2024, 8, 14),
-                  affectCalculation: true,
-                  formulaAttributes: {
-                    maxPoints: '',
-                    minRequiredPoints: '',
-                    weight: ''
-                  },
+                  daysValid: 100,
                   subAttainments: [],
                 },
                 {
                   id: 1132,
                   name: 'Exercise 3.2',
                   tag: 'test tag 6',
-                  category: 'Other',
-                  date: new Date(2023, 9, 1),
-                  expiryDate: new Date(2024, 8, 14),
-                  affectCalculation: true,
-                  formulaAttributes: {
-                    maxPoints: '',
-                    minRequiredPoints: '',
-                    weight: ''
-                  },
+                  daysValid: 100,
                   subAttainments: [],
                 }
               ]
@@ -109,15 +62,7 @@ const mockAttainmentsClient = {
               id: 114,
               name: 'Exercise 4',
               tag: 'test tag 7',
-              category: 'Other',
-              date: new Date(2023, 9, 1),
-              expiryDate: new Date(2024, 8, 14),
-              affectCalculation: true,
-              formulaAttributes: {
-                maxPoints: '',
-                minRequiredPoints: '',
-                weight: ''
-              },
+              daysValid: 100,
               subAttainments: [],
             }
           ]
@@ -126,60 +71,27 @@ const mockAttainmentsClient = {
           id: 12,
           name: '3 optional exercises',
           tag: 'test tag 8',
-          category: 'Other',
-          date: new Date(2023, 9, 1),
-          expiryDate: new Date(2024, 8, 14),
-          formulaId: 1,
-          affectCalculation: false,  // optional exercises don't affect the grade of Exercises
-          fformulaAttributes: {
-            maxPoints: '',
-            minRequiredPoints: '',
-            weight: ''
-          },
+          daysValid: 100,
           subAttainments: [
             {
               id: 121,
               name: 'Exercise 5',
               tag: 'test tag 9',
-              category: 'Other',
-              date: new Date(2023, 9, 1),
-              expiryDate: new Date(2024, 8, 14),
-              affectCalculation: true,  // The grade for optional exercises is still calculated
-              formulaAttributes: {
-                maxPoints: '',
-                minRequiredPoints: '',
-                weight: ''
-              },
+              daysValid: 100,
               subAttainments: [],
             },
             {
               id: 122,
               name: 'Exercise 6',
               tag: 'test tag 10',
-              category: 'Other',
-              date: new Date(2023, 9, 1),
-              expiryDate: new Date(2024, 8, 14),
-              affectCalculation: true,  // The grade for optional exercises is still calculated
-              formulaAttributes: {
-                maxPoints: '',
-                minRequiredPoints: '',
-                weight: ''
-              },
+              daysValid: 100,
               subAttainments: [],
             },
             {
               id: 123,
               name: 'Exercise 7',
               tag: 'test tag 11',
-              category: 'Other',
-              date: new Date(2023, 9, 1),
-              expiryDate: new Date(2024, 8, 14),
-              affectCalculation: true,  // The grade for optional exercses is still calculated
-              formulaAttributes: {
-                maxPoints: '',
-                minRequiredPoints: '',
-                weight: ''
-              },
+              daysValid: 100,
               subAttainments: [],
             },
           ]
@@ -190,18 +102,14 @@ const mockAttainmentsClient = {
       id: 2,
       name: 'Project',
       tag: 'test project',
-      category: 'Project',
-      date: new Date(2023, 9, 1),
-      expiryDate: new Date(2024, 8, 14),
+      daysValid: 100,
       subAttainments: [],
     },
     {
       id: 3,
       name: 'Exam',
       tag: 'test exam',
-      category: 'Exam',
-      date: new Date(2023, 9, 1),
-      expiryDate: new Date(2024, 8, 14),
+      daysValid: 100,
       subAttainments: [],
     }
   ]
