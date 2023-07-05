d73680d782c53c4d5d2e21be2f162a3fae0a14b2
Jere Stigell <jj.stigell@gmail.com>
2023-07-05 12:59:33 +0300

    update front tests

--- client/src/tests/mock-data/mockAttainments.tsx
+++ client/src/tests/mock-data/mockAttainments.tsx
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: MIT
 
-import { AttainmentData } from 'aalto-grades-common/types';
+import { AttainmentData, Formula } from 'aalto-grades-common/types';
 
 // These mock attainments are temporary and they will be modified later
 // They have been kept in order to not break any tests or content currently in main
@@ -12,18 +12,21 @@ const mockAttainments: Array<AttainmentData> = [
     id: 1,
     name: 'Exercises',
     tag: 'exercise',
+    formula: Formula.WeightedAverage,
     daysValid: 100
   },
   {
     id: 2,
     name: 'Projects',
     tag: 'project',
+    formula: Formula.WeightedAverage,
     daysValid: 1
   },
   {
     id: 3,
     name: 'Exams',
     tag: 'exam',
+    formula: Formula.WeightedAverage,
     daysValid: 365
   }
 ];
