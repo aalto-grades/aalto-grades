612e78c6b0c09b36c85dc2c33637a88288b8cce6
Jere Stigell <jj.stigell@gmail.com>
2023-07-10 14:35:04 +0300

    update user util

--- server/src/controllers/utils/user.ts
+++ server/src/controllers/utils/user.ts
@@ -5,7 +5,9 @@
 import User from '../../database/models/user';
 import TeacherInCharge from '../../database/models/teacherInCharge';
 
+import { SystemRole } from 'aalto-grades-common/types';
 import { ApiError } from '../../types/error';
+import { JwtClaims } from '../../types/general';
 import { HttpCode } from '../../types/httpCode';
 
 /**
@@ -24,18 +26,31 @@ export async function findUserById(userId: number, errorCode: HttpCode): Promise
   return user;
 }
 
-export async function isTeacherInCharge(
-  userId: number, courseId: number, errorCode: HttpCode
-): Promise<TeacherInCharge> {
+/**
+ * Function for checking is the user either admin on system level or
+ * teacher in charge on course level.
+ * @param {JwtClaims} user - User data from JWT.
+ * @param {number} courseId - The ID of the course.
+ * @param {HttpCode} errorCode - HTTP status code to return if the user access is denied.
+ * @returns {Promise<User>} - A promise that resolves with the found user model object.
+ * @throws {ApiError} - If the user is either admin or teacher in charge.
+ */
+export async function isTeacherInChargeOrAdmin(
+  user: JwtClaims, courseId: number, errorCode: HttpCode
+): Promise<void> {
+
+  if (user.role === SystemRole.Admin) {
+    return;
+  }
+
   const teacher: TeacherInCharge | null = await TeacherInCharge.findOne({
     where: {
-      userId,
+      userId: user.id,
       courseId
     }
   });
 
   if (!teacher) {
-    throw new ApiError(`user with ID ${userId} is not allowed not execute the action`, errorCode);
+    throw new ApiError(`user with ID ${user.id} is not allowed not execute the action`, errorCode);
   }
-  return teacher;
 }
