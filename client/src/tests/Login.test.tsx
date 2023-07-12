e613016a154f6a2d97e7011c48c79086af6e422d
jj-stigell <63514007+jj-stigell@users.noreply.github.com>
2023-07-12 17:46:08 +0900

    update login form (#352)
    * update form
    
    * Update client/src/components/auth/ExternalAuth.tsx
    
    Co-authored-by: no-comma <82644961+no-comma@users.noreply.github.com>
    
    * Update client/src/components/auth/LoginForm.tsx
    
    Co-authored-by: no-comma <82644961+no-comma@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: no-comma <82644961+no-comma@users.noreply.github.com>

--- client/src/tests/Login.test.tsx
+++ client/src/tests/Login.test.tsx
@@ -8,6 +8,9 @@ import { act, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import LoginForm from '../components/auth/LoginForm';
 import Login from '../components/auth/Login';
+import userServices from '../services/user';
+import { LoginResult } from 'aalto-grades-common/types';
+import { LoginCredentials } from '../types/auth';
 
 describe('Tests for Login and LoginForm components', () => {
 
@@ -19,21 +22,28 @@ describe('Tests for Login and LoginForm components', () => {
       </BrowserRouter>
     );
 
+    expect(screen.getByText('Log in to Aalto Grades')).toBeDefined();
+    expect(screen.getByText('Aalto University users')).toBeDefined();
+    expect(screen.getByText('Local users')).toBeDefined();
     expect(screen.getByLabelText('Email')).toBeDefined();
     expect(screen.getByLabelText('Password')).toBeDefined();
-    expect(screen.getByText('login')).toBeDefined();
+    expect(screen.getByText('log in')).toBeDefined();
     expect(screen.getByText('Don\'t have an account yet?')).toBeDefined();
   });
 
   test('LoginForm should allow a user to submit their credentials', () => {
+    const mockLoginUser: jest.SpyInstance<Promise<LoginResult>, [credentials: LoginCredentials]> =
+      jest.spyOn(userServices, 'login');
 
-    const mockLoginUser: jest.Mock = jest.fn();
-
-    render(<LoginForm loginUser={mockLoginUser}/>);
+    render(
+      <BrowserRouter>
+        <LoginForm />
+      </BrowserRouter>
+    );
 
     act(() => userEvent.type(screen.getByLabelText('Email'), 'test@email.com'));
     act(() => userEvent.type(screen.getByLabelText('Password'), 'secret'));
-    act(() => userEvent.click(screen.getByText('login')));
+    act(() => userEvent.click(screen.getByText('log in')));
 
     expect(mockLoginUser).toHaveBeenCalledTimes(1);
     expect(mockLoginUser).toHaveBeenCalledWith({
@@ -43,4 +53,3 @@ describe('Tests for Login and LoginForm components', () => {
   });
 
 });
-
