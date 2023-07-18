4a91265c49063d5f93674b3410fdbea4476fc232
no-comma <82644961+no-comma@users.noreply.github.com>
2023-07-18 10:36:46 +0300

    Remove signup form

--- client/src/components/auth/Signup.tsx
+++ client/src/components/auth/Signup.tsx
@@ -2,23 +2,25 @@
 //
 // SPDX-License-Identifier: MIT
 
-import { LoginResult } from 'aalto-grades-common/types';
-import { useTheme, Theme } from '@mui/material/styles';
-import { useState } from 'react';
+import { LoginResult, SystemRole } from 'aalto-grades-common/types';
+import { Button, FormControlLabel, Grid, Radio, RadioGroup, TextField } from '@mui/material';
+import { SyntheticEvent, useState } from 'react';
 import { NavigateFunction, useNavigate } from 'react-router-dom';
-
-import SignupForm from './SignupForm';
 
 import { useSignUp, UseSignUpResult } from '../../hooks/useApi';
 import useAuth, { AuthContextType } from '../../hooks/useAuth';
-import { SignupCredentials, State } from '../../types';
+import { State } from '../../types';
 
 export default function Signup(): JSX.Element {
 
   const navigate: NavigateFunction = useNavigate();
   const { setAuth }: AuthContextType = useAuth();
-  const theme: Theme = useTheme();
-  const [errorMessage, setErrorMessage]: State<string> = useState('');
+
+  const [name, setName]: State<string> = useState('');
+  const [password, setPassword]: State<string> = useState('');
+  const [email, setEmail]: State<string> = useState('');
+  const [studentNumber, setStudentNumber]: State<string> = useState<string>('');
+  const [role, setRole]: State<SystemRole> = useState<SystemRole>(SystemRole.User);
 
   const signUp: UseSignUpResult = useSignUp({
     onSuccess: (auth: LoginResult | null) => {
@@ -28,13 +30,93 @@ export default function Signup(): JSX.Element {
     }
   });
 
+  function handleSubmit(event: SyntheticEvent): void {
+    event.preventDefault();
+
+    signUp.mutate({
+      name: name,
+      password: password,
+      email: email,
+      role: role,
+      studentNumber: studentNumber.length > 0 ? studentNumber : undefined
+    });
+  }
+
   return (
     <div>
       <h1>Sign up</h1>
-      <p style={{ color: `${theme.palette.primary.dark}` }}>{errorMessage}</p>
-      <SignupForm
-        addUser={(credentials: SignupCredentials) => signUp.mutate(credentials)}
-      />
+      <form onSubmit={handleSubmit}>
+        <div>
+          <TextField
+            type='text'
+            value={name}
+            name='name'
+            label='Name'
+            onChange={(
+              { target }: { target: EventTarget & (HTMLInputElement | HTMLTextAreaElement) }
+            ): void => setName(target.value)}
+            InputLabelProps={{ shrink: true }}
+            margin='normal'
+          />
+        </div>
+        <div>
+          <TextField
+            type='email'
+            value={email}
+            name='email'
+            label='Email'
+            onChange={(
+              { target }: { target: EventTarget & (HTMLInputElement | HTMLTextAreaElement) }
+            ): void => setEmail(target.value)}
+            InputLabelProps={{ shrink: true }}
+            margin='normal'
+          />
+        </div>
+        <div>
+          <TextField
+            type='text'
+            value={studentNumber}
+            name='StudentNumber'
+            label='Student Number (not required)'
+            onChange={(
+              { target }: { target: EventTarget & (HTMLInputElement | HTMLTextAreaElement) }
+            ): void => setStudentNumber(target.value)}
+            InputLabelProps={{ shrink: true }}
+            margin='normal'
+          />
+        </div>
+        <div>
+          <TextField
+            type='password'
+            value={password}
+            name='password'
+            label='Password'
+            onChange={(
+              { target }: { target: EventTarget & (HTMLInputElement | HTMLTextAreaElement) }
+            ): void => setPassword(target.value)}
+            InputLabelProps={{ shrink: true }}
+            margin='normal'
+          />
+        </div>
+        <Grid
+          container
+          spacing={0}
+          direction='column'
+          alignItems='center'
+          justifyContent='center'
+        >
+          <RadioGroup
+            defaultValue={SystemRole.User}
+            name='radio-buttons-group'
+            onChange={(
+              { target }: { target: EventTarget & HTMLInputElement }
+            ): void => setRole(target.value as SystemRole)}>
+            <FormControlLabel value={SystemRole.User} control={<Radio />} label='User' />
+            <FormControlLabel value={SystemRole.Admin} control={<Radio />} label='Admin' />
+          </RadioGroup>
+        </Grid>
+        <Button type='submit'>sign up</Button>
+      </form>
     </div>
   );
 }
