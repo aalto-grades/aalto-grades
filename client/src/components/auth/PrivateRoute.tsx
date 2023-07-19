bb909fe340912af2047fd001d34000faa02137de
no-comma <82644961+no-comma@users.noreply.github.com>
2023-07-19 06:51:48 +0300

    Fix PrivateRoute, readd loading state

--- client/src/components/auth/PrivateRoute.tsx
+++ client/src/components/auth/PrivateRoute.tsx
@@ -7,11 +7,13 @@
 
 import { LoginResult, SystemRole } from 'aalto-grades-common/types';
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { UseQueryResult } from '@tanstack/react-query';
 
 import { useGetRefreshToken } from '../../hooks/useApi';
 import useAuth, { AuthContextType } from '../../hooks/useAuth';
+import { State } from '../../types';
 
 export default function PrivateRoute(props: {
   children?: JSX.Element,
@@ -19,16 +21,22 @@ export default function PrivateRoute(props: {
 }): JSX.Element | null {
 
   const { auth, setAuth, isTeacherInCharge }: AuthContextType = useAuth();
+  const [loading, setLoading]: State<boolean> = useState(true);
 
   const refresh: UseQueryResult<LoginResult> = useGetRefreshToken();
 
-  // only load page after token has been retrieved
-  if (!refresh.isLoading) {
-    setAuth(refresh.data ?? null);
+  useEffect(() => {
+    if (!refresh.isLoading) {
+      setAuth(refresh.data ?? null);
+      setLoading(false);
+    }
+  }, [refresh.isLoading]);
 
+  // Only load page after token has been retrieved
+  if (!loading) {
     // If auth is not null -> token exists
     if (auth) {
-      // check if role is in the list of authorised roles or teacher in charge.
+      // Check if role is in the list of authorised roles or teacher in charge.
       if (props.roles.includes(auth.role) || isTeacherInCharge) {
         return (
           <>
@@ -36,11 +44,11 @@ export default function PrivateRoute(props: {
             <Outlet />
           </>
         );
-      // if user is not authorised to access page -> redirect to front page
+      // If the user is not authorized to access a page -> redirect to front page
       } else {
         return <Navigate to='/' />;
       }
-    // no role found -> no token -> redirect to login
+    // No role found -> no token -> redirect to login
     } else {
       return <Navigate to='/login' />;
     }
