5ed61cdc01a4658f1ea9c1617201698424a1bc86
Jere Stigell <jj.stigell@gmail.com>
2023-07-19 15:51:45 +0300

    log requests middleware

---
+++ server/src/middleware/requestLogger.ts
@@ -0,0 +1,20 @@
+import { RequestHandler } from 'express';
+import morgan from 'morgan';
+
+import logger from '../configs/winston';
+
+/**
+ * Morgan formats incoming request to logger friendly format.
+ * Winston logs the data to files (depends on the NODE_ENV,
+ * see winston setup in ./configs).
+ * https://expressjs.com/en/resources/middleware/morgan.html
+ */
+export const requestLogger: RequestHandler = morgan(
+  ':remote-addr :remote-user ":method :url HTTP/:http-version"' +
+  ' :status :res[content-length] ":referrer" ":user-agent"',
+  {
+    stream: {
+      write: (message: string) => logger.http(message.trim())
+    }
+  }
+);
