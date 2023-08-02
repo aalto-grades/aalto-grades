c665695ea66c6ab30d73534ca077629543f50e98
no-comma <82644961+no-comma@users.noreply.github.com>
2023-08-02 09:32:11 +0300

    Fix tests

--- client/src/tests/EditCourseView.test.tsx
+++ client/src/tests/EditCourseView.test.tsx
@@ -9,17 +9,17 @@ import '@testing-library/jest-dom/extend-expect';
 import { act, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import CreateCourseView from '../components/CreateCourseView';
+import EditCourseView from '../components/EditCourseView';
 import { mockPostSuccess, server } from './mock-data/server';
 
-describe('Tests for CreateCourseView components', () => {
+describe('Tests for EditCourseView components', () => {
 
-  function renderCreateCourseView(): void {
+  function renderEditCourseView(): void {
     render(
       <QueryClientProvider client={new QueryClient()}>
-        <MemoryRouter initialEntries={['/create-course']}>
+        <MemoryRouter initialEntries={['/course/create']}>
           <Routes>
-            <Route path='/create-course' element={<CreateCourseView />} />
+            <Route path='/course/:modification/:courseId?' element={<EditCourseView />} />
           </Routes>
         </MemoryRouter>
       </QueryClientProvider>
@@ -27,10 +27,10 @@ describe('Tests for CreateCourseView components', () => {
   }
 
   test(
-    'CreateCourseView should render all of the appropriate components',
+    'EditCourseView should render all of the appropriate components',
     () => {
 
-      renderCreateCourseView();
+      renderEditCourseView();
 
       expect(screen.getByText('Create a New Course')).toBeDefined();
       expect(screen.getByLabelText('Course Code*')).toBeDefined();
@@ -45,13 +45,13 @@ describe('Tests for CreateCourseView components', () => {
       expect(screen.getByLabelText('Teachers In Charge*')).toBeDefined();
       expect(screen.getByText('Add')).toBeDefined();
       expect(screen.getByText('Cancel')).toBeDefined();
-      expect(screen.getByText('Create Course')).toBeDefined();
+      expect(screen.getByText('Submit')).toBeDefined();
     }
   );
 
-  test('CreateCourseForm should allow an admin to create a course', async () => {
+  test('EditCourseView should allow an admin to create a course', async () => {
 
-    renderCreateCourseView();
+    renderEditCourseView();
 
     const addCourse: jest.Mock = jest.fn();
     server.use(rest.post(
@@ -80,7 +80,7 @@ describe('Tests for CreateCourseView components', () => {
     act(() => userEvent.type(screen.getByLabelText('Teachers In Charge*'), testTeacher));
 
     act(() => userEvent.click(screen.getByText('Add')));
-    act(() => userEvent.click(screen.getByText('Create Course')));
+    act(() => userEvent.click(screen.getByText('Submit')));
 
     await waitFor(() => {
       expect(addCourse).toHaveBeenCalledTimes(1);
