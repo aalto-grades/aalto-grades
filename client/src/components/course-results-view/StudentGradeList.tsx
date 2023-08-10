9eac5e51a795df143d5dcc28d2e08b9cc4ee8263
Jere Stigell <jj.stigell@gmail.com>
2023-08-10 13:55:43 +0300

    conditional final grade edit

--- client/src/components/course-results-view/StudentGradeList.tsx
+++ client/src/components/course-results-view/StudentGradeList.tsx
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: MIT
 
 import { AttainmentGradeData, Status } from 'aalto-grades-common/types';
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Tooltip, Typography } from '@mui/material';
 import { JSX } from 'react';
 
 import StudentGradeAccordion from './StudentGradeAccordion';
@@ -32,14 +32,24 @@ export default function StudentGradeList(props: {
                 ? `${props.grades.grades[0].grade} - ${props.grades.grades[0].status}`
                 : Status.Pending
             )}
-            <Button
-              onClick={(): void => {
-                props.gradeToEdit(props.grades);
-              }}
-            >
-              Edit
-            </Button>
           </Typography>
+          <Tooltip
+            placement="top"
+            title={props.grades.grades.length == 0 ?
+              'To edit final grade, first calculate it.' :
+              ''}
+          >
+            <Box>
+              <Button
+                disabled={props.grades.grades.length == 0}
+                onClick={(): void => {
+                  props.gradeToEdit(props.grades);
+                }}
+              >
+                Edit
+              </Button>
+            </Box>
+          </Tooltip>
         </Box>
         {
           props.grades.subAttainments?.map((subAttainment: AttainmentGradeData) => (
