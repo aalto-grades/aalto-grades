771db8b471056e0d6afd55958aee37039e7a68df
Jere Stigell <jj.stigell@gmail.com>
2023-08-10 11:50:46 +0300

    update

--- client/src/components/course-results-view/EditGradeDialog.tsx
+++ client/src/components/course-results-view/EditGradeDialog.tsx
@@ -5,10 +5,10 @@
 import { AttainmentGradeData, EditGrade, GradeOption, Status } from 'aalto-grades-common/types';
 import { Form, Formik, FormikErrors, FormikTouched } from 'formik';
 import {
-  Box, Button, Dialog, DialogContent, DialogTitle, FormHelperText,
-  InputLabel, MenuItem, Select, SelectChangeEvent, TextField, Typography
+  Box, Button, Dialog, DialogContent, DialogTitle, InputLabel,
+  MenuItem, Select, SelectChangeEvent, TextField, Typography
 } from '@mui/material';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { Params, useParams } from 'react-router-dom';
 import * as yup from 'yup';
 
@@ -42,22 +42,6 @@ export default function EditGradeDialog(props: {
     comment: props.grade.grades[0].comment ?? ''
   });
 
-  function setToForm(id: number): void {
-    const toForm: GradeOption | undefined =
-      props.grade.grades.find((grade: GradeOption) => grade.gradeId == id);
-
-    if (toForm) {
-      setFormInitialValues({
-        grade: toForm.grade,
-        status: toForm.status,
-        date: toForm.date,
-        expiryDate: toForm.expiryDate,
-        comment: toForm.comment
-      });
-      setGradeId(toForm.gradeId as number);
-    }
-  }
-
   async function handleSubmit(values: EditGrade): Promise<void> {
     if (courseId && assessmentModelId && gradeId) {
       editGrade.mutate(
@@ -83,60 +67,64 @@ export default function EditGradeDialog(props: {
     }
   }
 
-  function closeDialog(): void {
-    props.setOpen(false);
-  }
+  useEffect(() => {
+    if (gradeId) {
+      const toForm: GradeOption = props.grade.grades.find(
+        (grade: GradeOption) => grade.gradeId == gradeId
+      ) as GradeOption;
+
+      setFormInitialValues({
+        grade: toForm.grade,
+        status: toForm.status,
+        date: toForm.date,
+        expiryDate: toForm.expiryDate,
+        comment: toForm.comment
+      });
+    }
+  }, [gradeId]);
 
   return (
     <>
       <Dialog open={props.open} transitionDuration={{ exit: 800 }}>
         <DialogTitle>Edit grade data for {props.grade.attainmentName}:</DialogTitle>
         <DialogContent sx={{ pb: 0 }}>
-          {(props.grade && props.grade.grades.length > 1) && (
-            <Box sx={{ mb: 2.5 }}>
-              <Box sx={{ mb: 1 }}>
-                Attainment has multiple grades, select grade for editing.
-              </Box>
-              <InputLabel id="assessment-model-select-helper-label">Select grade</InputLabel>
-              <Select
-                labelId="assessmentModelSelect"
-                id="assessmentModelSelectId"
-                value={String(gradeId)}
-                label="Assessment model"
-                fullWidth
-                onChange={(event: SelectChangeEvent): void => {
-                  setToForm(Number(event.target.value));
-                }}
-              >
-                {props.grade.grades.map((grade: GradeOption) => (
-                  <MenuItem key={grade.gradeId} value={grade.gradeId}>
-                    {grade.gradeId}
-                  </MenuItem>
-                ))}
-              </Select>
-              <FormHelperText>Select grade to edit.</FormHelperText>
-            </Box>
-          )}
+          <Box sx={{ mb: 2.5 }}>
+            <InputLabel id="grade-select-helper-label">
+              Select grade for editing
+            </InputLabel>
+            <Select
+              labelId="assessmentModelSelect"
+              id="assessmentModelSelectId"
+              value={String(gradeId)}
+              label="Assessment model"
+              fullWidth
+              onChange={(event: SelectChangeEvent): void => {
+                setGradeId(Number(event.target.value));
+              }}
+            >
+              {props.grade.grades.map((grade: GradeOption) => (
+                <MenuItem key={grade.gradeId} value={grade.gradeId}>
+                  {grade.gradeId}
+                </MenuItem>
+              ))}
+            </Select>
+          </Box>
           <Box>
-            {
-              (props.grade && props.grade.grades.length > 1) &&
-              <Typography variant='h3'>Edit Form</Typography>
-            }
-            {(formInitialValues) ? (
-              <Formik
-                enableReinitialize
-                initialValues={formInitialValues}
-                validationSchema={yup.object({
-                  grade: yup.number().min(0).notRequired(),
-                  status: yup.string().oneOf(Object.values(Status)).notRequired(),
-                  date: yup.date().notRequired(),
-                  expiryDate: yup.date().notRequired(),
-                  comment: yup.string().notRequired()
-                })}
-                onSubmit={handleSubmit}
-              >
-                {
-                  ({ errors, handleChange, isSubmitting, isValid, values, initialValues }:
+            <Typography variant='h3'>Edit Form</Typography>
+            <Formik
+              enableReinitialize
+              initialValues={formInitialValues}
+              validationSchema={yup.object({
+                grade: yup.number().min(0).notRequired(),
+                status: yup.string().oneOf(Object.values(Status)).notRequired(),
+                date: yup.date().notRequired(),
+                expiryDate: yup.date().notRequired(),
+                comment: yup.string().notRequired()
+              })}
+              onSubmit={handleSubmit}
+            >
+              {
+                ({ errors, handleChange, isSubmitting, isValid, values, initialValues }:
                 {
                   errors: FormikErrors<EditGrade>,
                   handleChange: (e: ChangeEvent<Element>) => void,
@@ -146,152 +134,149 @@ export default function EditGradeDialog(props: {
                   values: EditGrade,
                   initialValues: EditGrade
                 }
-                  ): JSX.Element => (
-                    <Form>
-                      <TextField
-                        id="grade"
-                        name="grade"
-                        type="text"
-                        fullWidth
-                        value={values.grade}
-                        disabled={isSubmitting}
-                        label="Grade"
-                        InputLabelProps={{ shrink: true }}
-                        margin='normal'
-                        helperText={
-                          errors.grade
+                ): JSX.Element => (
+                  <Form>
+                    <TextField
+                      id="grade"
+                      name="grade"
+                      type="text"
+                      fullWidth
+                      value={values.grade}
+                      disabled={isSubmitting}
+                      label="Grade"
+                      InputLabelProps={{ shrink: true }}
+                      margin='normal'
+                      helperText={
+                        errors.grade
                         ?? 'The numerical grade of a student for an attainment.'
-                        }
-                        error={Boolean(errors.grade)}
-                        onChange={handleChange}
-                      />
-                      <TextField
-                        id="status"
-                        name="status"
-                        type="text"
-                        fullWidth
-                        value={values.status}
+                      }
+                      error={Boolean(errors.grade)}
+                      onChange={handleChange}
+                    />
+                    <TextField
+                      id="status"
+                      name="status"
+                      type="text"
+                      fullWidth
+                      value={values.status}
+                      disabled={isSubmitting}
+                      label="Status"
+                      InputLabelProps={{ shrink: true }}
+                      margin='normal'
+                      helperText={
+                        errors.status
+                          ? String(errors.status)
+                          : 'Grading status indicator.'
+                      }
+                      error={Boolean(errors.status)}
+                      onChange={handleChange}
+                      select
+                    >
+                      {
+                        Object.values(Status).map((value: Status) => {
+                          return (
+                            <MenuItem key={value} value={value}>{value}</MenuItem>
+                          );
+                        })
+                      }
+                    </TextField>
+                    <TextField
+                      id="date"
+                      name="date"
+                      type="date"
+                      fullWidth
+                      value={new Date(values.date as Date).toISOString().split('T')[0]}
+                      disabled={isSubmitting}
+                      label="Grade Date"
+                      InputLabelProps={{ shrink: true }}
+                      margin='normal'
+                      helperText={
+                        errors.date
+                          ? String(errors.date)
+                          : 'Date when attainment is completed (e.g., deadline or exam date)'
+                      }
+                      error={Boolean(errors.date)}
+                      onChange={handleChange}
+                    />
+                    <TextField
+                      id="expiryDate"
+                      name="expiryDate"
+                      type="date"
+                      fullWidth
+                      value={new Date(values.expiryDate as Date).toISOString().split('T')[0]}
+                      disabled={isSubmitting}
+                      label="Grade Expiry date"
+                      InputLabelProps={{ shrink: true }}
+                      margin='normal'
+                      helperText={
+                        errors.expiryDate
+                          ? String(errors.expiryDate)
+                          : 'Date when the grade expires.'
+                      }
+                      error={Boolean(errors.expiryDate)}
+                      onChange={handleChange}
+                    />
+                    <TextField
+                      id="comment"
+                      name="comment"
+                      type="text"
+                      fullWidth
+                      value={values.comment}
+                      disabled={isSubmitting}
+                      label="comment"
+                      InputLabelProps={{ shrink: true }}
+                      margin='normal'
+                      helperText={
+                        errors.comment
+                          ? String(errors.comment)
+                          : 'Comment for grade.'
+                      }
+                      error={Boolean(errors.comment)}
+                      onChange={handleChange}
+                    />
+                    <Box sx={{
+                      display: 'flex',
+                      flexWrap: 'wrap',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                      pt: 2,
+                      pb: 3
+                    }}>
+                      <Button
+                        size='medium'
+                        variant='outlined'
+                        color={initialValues != values ? 'error' : 'primary'}
                         disabled={isSubmitting}
-                        label="Status"
-                        InputLabelProps={{ shrink: true }}
-                        margin='normal'
-                        helperText={
-                          errors.status
-                            ? String(errors.status)
-                            : 'Grading status indicator.'
-                        }
-                        error={Boolean(errors.status)}
-                        onChange={handleChange}
-                        select
+                        onClick={(): void => {
+                          if (initialValues != values) {
+                            setShowDialog(true);
+                          } else {
+                            props.setOpen(false);
+                          }
+                        }}
                       >
-                        {
-                          Object.values(Status).map((value: Status) => {
-                            return (
-                              <MenuItem key={value} value={value}>{value}</MenuItem>
-                            );
-                          })
-                        }
-                      </TextField>
-                      <TextField
-                        id="date"
-                        name="date"
-                        type="date"
-                        fullWidth
-                        value={new Date(values.date as Date).toISOString().split('T')[0]}
-                        disabled={isSubmitting}
-                        label="Grade Date"
-                        InputLabelProps={{ shrink: true }}
-                        margin='normal'
-                        helperText={
-                          errors.date
-                            ? String(errors.date)
-                            : 'Date when attainment is completed (e.g., deadline or exam date)'
-                        }
-                        error={Boolean(errors.date)}
-                        onChange={handleChange}
-                      />
-                      <TextField
-                        id="expiryDate"
-                        name="expiryDate"
-                        type="date"
-                        fullWidth
-                        value={new Date(values.expiryDate as Date).toISOString().split('T')[0]}
-                        disabled={isSubmitting}
-                        label="Grade Expiry date"
-                        InputLabelProps={{ shrink: true }}
-                        margin='normal'
-                        helperText={
-                          errors.expiryDate
-                            ? String(errors.expiryDate)
-                            : 'Date when the grade expires.'
-                        }
-                        error={Boolean(errors.expiryDate)}
-                        onChange={handleChange}
-                      />
-                      <TextField
-                        id="comment"
-                        name="comment"
-                        type="text"
-                        fullWidth
-                        value={values.comment}
-                        disabled={isSubmitting}
-                        label="comment"
-                        InputLabelProps={{ shrink: true }}
-                        margin='normal'
-                        helperText={
-                          errors.comment
-                            ? String(errors.comment)
-                            : 'Comment for grade.'
-                        }
-                        error={Boolean(errors.comment)}
-                        onChange={handleChange}
-                      />
-                      <Box sx={{
-                        display: 'flex',
-                        flexWrap: 'wrap',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                        pt: 2,
-                        pb: 3
-                      }}>
-                        <Button
-                          size='medium'
-                          variant='outlined'
-                          color={initialValues != values ? 'error' : 'primary'}
-                          disabled={isSubmitting}
-                          onClick={(): void => {
-                            if (initialValues != values) {
-                              setShowDialog(true);
-                            } else {
-                              closeDialog();
-                            }
-                          }}
-                        >
-                          Close
-                        </Button>
-                        <Button
-                          id='ag_confirm_instance_details_btn'
-                          variant='contained'
-                          type='submit'
-                          disabled={!isValid || isSubmitting || initialValues == values}
-                        >
-                          Submit
-                        </Button>
-                      </Box>
-                    </Form>
-                  )
-                }
-              </Formik>
-            ) : (
-              <>Something went wrong</>
-            )}
+                        Close
+                      </Button>
+                      <Button
+                        id='ag_confirm_instance_details_btn'
+                        variant='contained'
+                        type='submit'
+                        disabled={!isValid || isSubmitting || initialValues == values}
+                      >
+                        Submit
+                      </Button>
+                    </Box>
+                  </Form>
+                )
+              }
+            </Formik>
           </Box>
         </DialogContent>
       </Dialog>
       <UnsavedChangesDialog
         setOpen={setShowDialog}
         open={showDialog}
-        handleDiscard={(): void => closeDialog()}
+        handleDiscard={(): void => props.setOpen(false)}
       />
       <AlertSnackbar snackPack={snackPack} />
     </>
