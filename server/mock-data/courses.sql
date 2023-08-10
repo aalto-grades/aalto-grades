ebe48d1cba24a2fcbffc0fc07fad6fcbe67a6893
Jere Stigell <jj.stigell@gmail.com>
2023-08-10 12:47:59 +0300

    mock

--- server/mock-data/courses.sql
+++ server/mock-data/courses.sql
@@ -2,15 +2,15 @@
 --
 -- SPDX-License-Identifier: MIT
 
-INSERT INTO public.course (course_code, min_credits, max_credits, grading_scale, created_at, updated_at) VALUES
-('CS-A1110', 5, 5, 'NUMERICAL', NOW(), NOW()),
-('CS-A1120', 5, 5, 'NUMERICAL', NOW(), NOW()),
-('PHYS-A1130', 5, 5, 'NUMERICAL', NOW(), NOW()),
-('TU-A1100', 5, 5, 'NUMERICAL', NOW(), NOW()),
-('CS-A1150', 5, 5, 'NUMERICAL', NOW(), NOW()),
-('MS-A0102', 5, 5, 'NUMERICAL', NOW(), NOW()),
-('CS-E4580', 5, 5, 'NUMERICAL', NOW(), NOW()),
-('ELEC-A7100', 5, 5, 'NUMERICAL', NOW(), NOW()),
-('PHYS-A1140', 5, 5, 'NUMERICAL', NOW(), NOW()),
-('Test edit course', 5, 5, 'PASS_FAIL', NOW(), NOW()),
-('Test edit course and teachers', 5, 5, 'PASS_FAIL', NOW(), NOW());
+INSERT INTO public.course (course_code, min_credits, max_credits, grading_scale, language_of_instruction, created_at, updated_at) VALUES
+('CS-A1110', 5, 5, 'NUMERICAL', 'EN', NOW(), NOW()),
+('CS-A1120', 5, 5, 'NUMERICAL', 'EN', NOW(), NOW()),
+('PHYS-A1130', 5, 5, 'NUMERICAL', 'EN', NOW(), NOW()),
+('TU-A1100', 5, 5, 'NUMERICAL', 'EN', NOW(), NOW()),
+('CS-A1150', 5, 5, 'NUMERICAL', 'EN', NOW(), NOW()),
+('MS-A0102', 5, 5, 'NUMERICAL', 'EN', NOW(), NOW()),
+('CS-E4580', 5, 5, 'NUMERICAL', 'EN', NOW(), NOW()),
+('ELEC-A7100', 5, 5, 'NUMERICAL', 'EN', NOW(), NOW()),
+('PHYS-A1140', 5, 5, 'NUMERICAL', 'EN', NOW(), NOW()),
+('Test edit course', 5, 5, 'PASS_FAIL', 'EN', NOW(), NOW()),
+('Test edit course and teachers', 5, 5, 'PASS_FAIL', 'EN', NOW(), NOW());
