f50e070daeb67bbc90d8f6cf07187a7b949e6d05
no-comma <82644961+no-comma@users.noreply.github.com>
2023-08-17 09:39:48 +0300

    Function to find date of latest grade

--- server/src/controllers/utils/date.ts
+++ server/src/controllers/utils/date.ts
@@ -4,6 +4,9 @@
 
 import { DateOnlyString } from 'aalto-grades-common/types';
 
+import Attainment from '../../database/models/attainment';
+import AttainmentGrade from '../../database/models/attainmentGrade';
+
 export function toDateOnlyString(date: Date | string): DateOnlyString {
   const dateOnlyRegExp: RegExp =
     /^\d{4}[/-](0?[1-9]|1[012])[/-](0?[1-9]|[12][0-9]|3[01])$/;
@@ -20,4 +23,43 @@ export function toDateOnlyString(date: Date | string): DateOnlyString {
     return validate(date.toISOString().split('T')[0]);
 
   return toDateOnlyString(new Date(date));
+}
+
+export async function getDateOfLatestGrade(
+  userId: number, assessmentModelId: number
+): Promise<Date> {
+  const grades: Array<AttainmentGrade> = await AttainmentGrade.findAll({
+    where: {
+      userId: userId,
+      manual: true
+    },
+    include: [
+      {
+        model: Attainment,
+        where: {
+          assessmentModelId: assessmentModelId
+        }
+      }
+    ]
+  });
+
+  const dates: Array<Date> = grades.map(
+    (grade: AttainmentGrade) => new Date(grade.date)
+  );
+
+  let maxSoFar: Date | null = null;
+  for (const date of dates) {
+    if (!maxSoFar || date > maxSoFar) {
+      maxSoFar = date;
+    }
+  }
+
+  if (maxSoFar) {
+    return maxSoFar;
+  } else {
+    throw new Error(
+      `failed to find the date of the latest grade, user ${userId} has`
+      + ` no grades for assessment model ${assessmentModelId}`
+    )
+  }
 }
---
+++ server/test/functions/latestDate.test.ts
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: 2023 The Aalto Grades Developers
+//
+// SPDX-License-Identifier: MIT
+
+import { getDateOfLatestGrade } from '../../src/controllers/utils/date';
+
+describe('Test latest date finder', () => {
+
+  it('should return the correct date', async () => {
+    expect(await getDateOfLatestGrade(391, 49)).toEqual(new Date('2022-07-01'));
+    expect(await getDateOfLatestGrade(1, 8)).toEqual(new Date('2023-01-01'));
+  });
+
+  it('should not return a timestamp', async () => {
+    expect(
+      (await getDateOfLatestGrade(391, 49)).toISOString().split('T')[1]
+    ).toEqual('00:00:00.000Z');
+    expect(
+      (await getDateOfLatestGrade(1, 8)).toISOString().split('T')[1]
+    ).toEqual('00:00:00.000Z');
+  });
+
+  it('should throw an error when no grades exist', async () => {
+    await expect(getDateOfLatestGrade(500, 1)).rejects.toThrow();
+  });
+});
