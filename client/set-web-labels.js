0a5240be558da106854b598251ce602643899a39
no-comma <82644961+no-comma@users.noreply.github.com>
2023-08-18 08:15:19 +0300

    Update set-web-labels to identify licenses

--- client/set-web-labels.js
+++ client/set-web-labels.js
@@ -9,8 +9,11 @@
  * modifying the table in the build.
  */
 
+const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const checker = require('license-checker-rseidelsohn');
+const parse = require('spdx-expression-parse');
 
 const jsFilename = `${process.cwd()}/build/javascript.html`;
 const jsBuffer = fs.readFileSync(jsFilename);
@@ -22,4 +25,83 @@ const $ = cheerio.load(jsBuffer);
 $('#main').attr('href', `/static/js/${main}`);
 $('#main').text(main);
 
-fs.writeFileSync(jsFilename, $.html());
+async function addLicenses() {
+  const libreJsLicenseList = (await axios.get(
+    'https://git.savannah.gnu.org/cgit/librejs.git/plain/common/license_definitions.json'
+  )).data;
+
+  const spdxLicenseList = (await axios.get(
+    'https://raw.githubusercontent.com/spdx/license-list-data/main/json/licenses.json'
+  )).data.licenses;
+
+  function traverseSpdxExpr(addLicenseData, expr) {
+    if (expr.license) {
+      const spdx = spdxLicenseList.find((val) => val.licenseId === expr.license);
+      if (spdx) {
+        if (spdx.isFsfLibre) {
+          addLicenseData(spdx.licenseId, spdx.reference);
+        } else {
+          console.error(`Encountered NON-FREE license: ${spdx.licenseId}, ${spdx.name}`);
+        }
+      } else {
+        console.error(`Encountered unknown license: ${expr.license}`);
+      }
+    }
+
+    if (expr.left) {
+      traverseSpdxExpr(addLicenseData, expr.left);
+    }
+
+    if (expr.right) {
+      traverseSpdxExpr(addLicenseData, expr.right);
+    }
+  }
+
+  checker.init(
+    {
+      start: '.',
+      excludePrivatePackages: true,
+      production: true
+    },
+    (error, packages) => {
+      if (error) {
+        console.error(error.message);
+      } else {
+        const licenseData = {
+          idSet: new Set(),
+          urlMap: new Map()
+        };
+
+        function addLicenseData(id, url) {
+          if (id !== 'MIT' && !licenseData.idSet.has(id)) {
+            licenseData.idSet.add(id);
+            licenseData.urlMap.set(id, url);
+          }
+        }
+
+        for (const key of Object.keys(packages)) {
+          const packageLicense = packages[key].licenses;
+
+          const libreJs = libreJsLicenseList[packageLicense];
+          if (libreJs) {
+            addLicenseData(libreJs.identifier, libreJs.canonicalUrl[0]);
+          } else {
+            try {
+              traverseSpdxExpr(addLicenseData, parse(packageLicense));
+            } catch (parseError) {
+              console.error(`Encountered unknown license: ${packageLicense}`);
+            }
+          }
+        }
+
+        for (const [id, url] of licenseData.urlMap.entries()) {
+          $(`<a href="${url}">${id}</a>\n`).appendTo('#licenses');
+        }
+      }
+
+      fs.writeFileSync(jsFilename, $.html());
+    }
+  )
+}
+
+addLicenses();
