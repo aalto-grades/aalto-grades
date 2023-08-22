0f367e206038a673066a61b4743ac21355eddc43
Jere Stigell <jj.stigell@gmail.com>
2023-08-22 11:48:22 +0300

    add db constraint

---
+++ server/src/database/migrations/00008-completion-expiry-date-constraint.ts
@@ -0,0 +1,36 @@
+// SPDX-FileCopyrightText: 2023 The Aalto Grades Developers
+//
+// SPDX-License-Identifier: MIT
+
+import { QueryInterface, Op } from 'sequelize';
+
+import { sequelize } from '..';
+import logger from '../../configs/winston';
+
+export default {
+  up: async (queryInterface: QueryInterface): Promise<void> => {
+    try {
+      await queryInterface.addConstraint('attainment_grade', {
+        fields: ['date'],
+        type: 'check',
+        where: {
+          date: {
+            [Op.lte]: sequelize.col('expiry_date')
+          }
+        }
+      });
+    } catch (error) {
+      logger.error(error);
+    }
+  },
+  down: async (queryInterface: QueryInterface): Promise<void> => {
+    try {
+      await queryInterface.removeConstraint(
+        'attainment_grade',
+        'attainment_grade_date_ck'
+      );
+    } catch (error) {
+      logger.error(error);
+    }
+  },
+};
