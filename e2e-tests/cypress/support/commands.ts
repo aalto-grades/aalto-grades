dfdae3d863a9e645fcf90bc6f1059d73abf17734
Jere Stigell <jj.stigell@gmail.com>
2023-08-24 09:51:40 +0300

    lint e2e

--- e2e-tests/cypress/support/commands.ts
+++ e2e-tests/cypress/support/commands.ts
@@ -35,7 +35,9 @@
 //       login(email: string, password: string): Chainable<void>
 //       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
 //       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+//       visit(
+//        originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>
+//       ): Chainable<Element>
 //     }
 //   }
 // }
