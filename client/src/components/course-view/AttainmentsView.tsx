8cf641ce802343d4887b9ea1c3321caf92c7da46
gubo97000 <gubo97000@hotmail.it>
2024-04-17 15:03:02 +0300

    table-view

--- client/src/components/course-view/AttainmentsView.tsx
+++ client/src/components/course-view/AttainmentsView.tsx
@@ -3,12 +3,29 @@
 // SPDX-License-Identifier: MIT
 
 import {AttainmentData} from '@common/types';
-import {Box, Button, Chip, IconButton, Typography} from '@mui/material';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Chip,
+  IconButton,
+  Typography,
+} from '@mui/material';
 import {JSX, useState} from 'react';
 import {Params, useParams} from 'react-router-dom';
 
-import {AccessTime, Delete, Tag} from '@mui/icons-material';
+import {
+  AccessTime,
+  Delete,
+  TableRows,
+  TableRowsOutlined,
+  Tag,
+  Window,
+  WindowOutlined,
+} from '@mui/icons-material';
+import {DataGrid, GridActionsCellItem, GridColDef} from '@mui/x-data-grid';
 import {useDeleteAttainment, useGetAttainments} from '../../hooks/useApi';
+import {Numeric} from '../../types';
 import NewAttainmentDialog from './NewAttainmentDialog';
 
 export default function CourseView(): JSX.Element {
@@ -16,74 +33,166 @@ export default function CourseView(): JSX.Element {
   const deleteAttainment = useDeleteAttainment();
   const attainments = useGetAttainments(courseId);
   const [addDialogOpen, setAddDialogOpen] = useState(false);
+  const [layout, setLayout] = useState<'table' | 'grid'>('table');
+  // const {openConfirmDialog} = useConfirmDialog();
+
+  const handleConfirmDelete = (attId: Numeric) => {
+    deleteAttainment.mutate({
+      courseId,
+      attainmentId: attId,
+    });
+  };
+
+  const columns: GridColDef[] = [
+    {
+      field: 'id',
+      headerName: 'ID',
+      type: 'string',
+    },
+    {
+      field: 'name',
+      headerName: 'Title',
+      type: 'string',
+      width: 120,
+      editable: true,
+    },
+    {
+      field: 'daysValid',
+      headerName: 'Days valid',
+      type: 'number',
+      width: 120,
+      editable: true,
+    },
+    {
+      field: 'dateValid',
+      headerName: 'Date valid',
+      type: 'date',
+      width: 120,
+      editable: true,
+    },
+    {
+      field: 'actions',
+      type: 'actions',
+      getActions: params => [
+        <GridActionsCellItem
+          icon={<Delete />}
+          label="Delete"
+          onClick={() => handleConfirmDelete(params.id)}
+        />,
+      ],
+    },
+  ];
 
   return (
     <>
-      <Button onClick={() => setAddDialogOpen(true)}>Add attainment</Button>
+      <Box sx={{display: 'flex', mb: 1}}>
+        <Button onClick={() => setAddDialogOpen(true)}>Add attainment</Button>
+        <ButtonGroup>
+          <Button onClick={() => setLayout('table')}>
+            {layout === 'table' ? <TableRows /> : <TableRowsOutlined />}
+          </Button>
+          <Button onClick={() => setLayout('grid')}>
+            {layout === 'grid' ? <Window /> : <WindowOutlined />}
+          </Button>
+        </ButtonGroup>
+      </Box>
+
+      {layout === 'grid' && (
+        <Box
+          style={{
+            display: 'flex',
+            width: '100%',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexWrap: 'wrap',
+            gap: '1rem',
+          }}
+        >
+          {attainments.data &&
+            attainments.data.map((attainment: AttainmentData) => (
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                key={attainment.id}
+              >
+                <Box
+                  sx={{
+                    border: 1,
+                    borderColor: 'info.grey',
+                    borderRadius: 1,
+                    p: 1,
+                  }}
+                >
+                  <Typography sx={{py: 1.7}}>{attainment.name}</Typography>
+                  <Chip
+                    icon={<Tag />}
+                    label={`${attainment.id}`}
+                    variant="outlined"
+                    size="small"
+                    sx={{mr: 1}}
+                  />
+                  <Chip
+                    icon={<AccessTime />}
+                    label={`${attainment.daysValid} days`}
+                    variant="outlined"
+                    size="small"
+                  />
+
+                  <IconButton
+                    onClick={() => handleConfirmDelete(attainment.id)}
+                    aria-description="delete attainment"
+                  >
+                    <Delete />
+                  </IconButton>
+                </Box>
+              </Box>
+            ))}
+        </Box>
+      )}
+      {layout === 'table' && (
+        <DataGrid
+          rows={attainments.data || []}
+          columns={columns}
+          rowHeight={25}
+          editMode="row"
+          rowSelection={false}
+          disableColumnSelector
+          // slots={{toolbar: dataGridToolbar}}
+          sx={{maxHeight: '70vh', minHeight: '20vh'}}
+          // onRowEditStart={() => setEditing(true)}
+          // onRowEditStop={() => setEditing(false)}
+          // processRowUpdate={updatedRow => {
+          //   setRows(oldRows =>
+          //     oldRows.map(row => (row.id === updatedRow.id ? updatedRow : row))
+          //   );
+          //   // TODO: do some validation. Code below is an example.
+          //   // for (const [key, val] of Object.entries(updatedRow)) {
+          //   //   if (key === 'id' || key === 'StudentNo') continue;
+          //   //   if ((val as number) < 0)
+          //   //     throw new Error('Value cannot be negative');
+          //   //   else if ((val as number) > 5000)
+          //   //     throw new Error('Value cannot be over 5000');
+          //   // }
+          //   // setSnackBar({message: 'Row saved!', severity: 'success'});
+          //   setError(false);
+          //   return updatedRow;
+          // }}
+          // onProcessRowUpdateError={error => {
+          //   setError(true);
+          //   setSnackBar({message: error.message, severity: 'error'});
+          // }}
+        />
+      )}
+
       <NewAttainmentDialog
         handleClose={() => setAddDialogOpen(false)}
         open={addDialogOpen}
       />
-      <Box
-        style={{
-          display: 'flex',
-          width: '100%',
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexWrap: 'wrap',
-          gap: '1rem',
-        }}
-      >
-        {attainments.data &&
-          attainments.data.map((attainment: AttainmentData) => (
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-              key={attainment.id}
-            >
-              <Box
-                sx={{
-                  border: 1,
-                  borderColor: 'info.grey',
-                  borderRadius: 1,
-                  p: 1,
-                }}
-              >
-                <Typography sx={{py: 1.7}}>{attainment.name}</Typography>
-                <Chip
-                  icon={<Tag />}
-                  label={`${attainment.id}`}
-                  variant="outlined"
-                  size="small"
-                  sx={{mr: 1}}
-                />
-                <Chip
-                  icon={<AccessTime />}
-                  label={`${attainment.daysValid} days`}
-                  variant="outlined"
-                  size="small"
-                />
-
-                <IconButton
-                  onClick={() =>
-                    deleteAttainment.mutate({
-                      courseId,
-                      attainmentId: attainment.id,
-                    })
-                  }
-                  aria-description="delete attainment"
-                >
-                  <Delete />
-                </IconButton>
-              </Box>
-            </Box>
-          ))}
-      </Box>
     </>
   );
 }
