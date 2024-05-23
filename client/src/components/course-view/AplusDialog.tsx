b57f6d1c6dc9789eb07111dea468965ad1510a2b
no-comma <82644961+no-comma@users.noreply.github.com>
2024-05-23 15:34:16 +0300

    Update grade source selection logic

--- client/src/components/course-view/AplusDialog.tsx
+++ client/src/components/course-view/AplusDialog.tsx
@@ -2,135 +2,38 @@
 //
 // SPDX-License-Identifier: MIT
 
-import {ArrowDropDown} from '@mui/icons-material';
 import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
   Button,
   Card,
   CardContent,
-  Checkbox,
   Dialog,
   DialogActions,
   DialogContent,
   DialogTitle,
-  FormControlLabel,
-  FormGroup,
   TextField,
 } from '@mui/material';
 import {useState} from 'react';
 
-import {AplusCourseData} from '@/common/types';
 import {
-  useFetchAplusCourses,
-  useFetchAplusExerciseData,
-} from '../../hooks/useApi';
+  AplusCourseData,
+  AplusGradeSourceData,
+  AplusGradeSourceType,
+} from '@/common/types';
+import SelectAplusGradeSources from './SelectAplusGradeSources';
+import {useFetchAplusCourses} from '../../hooks/useApi';
 
-type AplusModule = {
-  id: number;
-  name: string;
-};
-
-type SelectAplusGradeSourcesProps = {
-  aplusCourse: AplusCourseData;
-  handleFullPointsChange: (checked: boolean) => void;
-  handleModuleChange: (module: AplusModule, checked: boolean) => void;
-  handleDifficultyChange: (difficulty: string, checked: boolean) => void;
-};
-
-const SelectAplusGradeSources = ({
-  aplusCourse,
-  handleFullPointsChange,
-  handleModuleChange,
-  handleDifficultyChange,
-}: SelectAplusGradeSourcesProps): JSX.Element => {
-  const aplusExerciseData = useFetchAplusExerciseData(aplusCourse.id);
-
-  return (
-    <>
-      {aplusExerciseData.data && (
-        <>
-          <FormControlLabel
-            control={
-              <Checkbox
-                onChange={event => handleFullPointsChange(event.target.checked)}
-              />
-            }
-            label="Full points"
-          />
-          <Accordion>
-            <AccordionSummary expandIcon={<ArrowDropDown />}>
-              Modules
-            </AccordionSummary>
-            <AccordionDetails>
-              <FormGroup>
-                {aplusExerciseData.data.modules.map(module => (
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        onChange={event =>
-                          handleModuleChange(module, event.target.checked)
-                        }
-                      />
-                    }
-                    label={module.name}
-                  />
-                ))}
-              </FormGroup>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion>
-            <AccordionSummary expandIcon={<ArrowDropDown />}>
-              Difficulties
-            </AccordionSummary>
-            <AccordionDetails>
-              <FormGroup>
-                {aplusExerciseData.data.difficulties.map(difficulty => (
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        onChange={event =>
-                          handleDifficultyChange(
-                            difficulty,
-                            event.target.checked
-                          )
-                        }
-                      />
-                    }
-                    label={difficulty}
-                  />
-                ))}
-              </FormGroup>
-            </AccordionDetails>
-          </Accordion>
-        </>
-      )}
-    </>
-  );
-};
+import Type = AplusGradeSourceType;
 
 type AttainmentCardProps = {
-  name: string;
+  suggestedName: string;
 };
 
-const AttainmentCard = ({
-  name,
-}: AttainmentCardProps): JSX.Element => {
+const AttainmentCard = ({suggestedName}: AttainmentCardProps): JSX.Element => {
   return (
     <Card>
       <CardContent>
-        <TextField
-          sx={{mt: 1}}
-          label="Name"
-          value={name}
-        />
-        <TextField
-          sx={{mt: 1}}
-          label="Days valid"
-          type="number"
-          value={365}
-        />
+        <TextField sx={{mt: 1}} label="Name" value={suggestedName} />
+        <TextField sx={{mt: 1}} label="Days valid" type="number" value={365} />
       </CardContent>
     </Card>
   );
@@ -138,34 +41,21 @@ const AttainmentCard = ({
 
 type CreateAplusAttainmentsProps = {
   aplusCourseId: number;
-  createFullPoints: boolean;
-  createModules: AplusModule[];
-  createDifficulties: string[];
+  attainmentsWithSource: [
+    {name: string; daysValid: number},
+    AplusGradeSourceData,
+  ][];
 };
 
 const CreateAplusAttainments = ({
   aplusCourseId,
-  createFullPoints,
-  createModules,
-  createDifficulties,
+  attainmentsWithSource,
 }: CreateAplusAttainmentsProps): JSX.Element => {
   return (
     <>
-      {createFullPoints && (
-        <AttainmentCard
-          name="A+ Course"
-        />
-      )}
-      {createModules.map(module =>
-        <AttainmentCard
-          name={module.name}
-        />
-      )}
-      {createDifficulties.map(difficulty =>
-        <AttainmentCard
-          name={`A+ Difficulty ${difficulty}`}
-        />
-      )}
+      {attainmentsWithSource.map(([attainment, source]) => (
+        <AttainmentCard suggestedName={attainment.name} />
+      ))}
     </>
   );
 };
@@ -181,9 +71,40 @@ const AplusDialog = ({handleClose, open}: PropsType): JSX.Element => {
   const [step, setStep] = useState<number>(0);
   const [aplusCourse, setAplusCourse] = useState<AplusCourseData | null>(null);
 
-  const [createFullPoints, setCreateFullPoints] = useState<boolean>(false);
-  const [createModules, setCreateModules] = useState<AplusModule[]>([]);
-  const [createDifficulties, setCreateDifficulties] = useState<string[]>([]);
+  const [attainmentsWithSource, setAttainmentsWithSource] = useState<
+    [{name: string; daysValid: number}, AplusGradeSourceData][]
+  >([]);
+
+  const handleSelectionChange = (
+    checked: boolean,
+    name: string,
+    source: AplusGradeSourceData
+  ): void => {
+    if (checked) {
+      setAttainmentsWithSource([
+        ...attainmentsWithSource,
+        [{name: name, daysValid: 365}, source],
+      ]);
+    } else {
+      setAttainmentsWithSource(
+        attainmentsWithSource.filter(([_, s]) => {
+          if (s.sourceType === Type.Module && source.sourceType === Type.Module)
+            return s.moduleId !== source.moduleId;
+
+          if (
+            s.sourceType === Type.Difficulty &&
+            source.sourceType === Type.Difficulty
+          )
+            return s.difficulty !== source.difficulty;
+
+          return !(
+            s.sourceType === Type.FullPoints &&
+            source.sourceType === Type.FullPoints
+          );
+        })
+      );
+    }
+  };
 
   return (
     <Dialog open={open} onClose={handleClose}>
@@ -204,42 +125,32 @@ const AplusDialog = ({handleClose, open}: PropsType): JSX.Element => {
         {step === 1 && aplusCourse && (
           <SelectAplusGradeSources
             aplusCourse={aplusCourse}
-            handleFullPointsChange={checked => setCreateFullPoints(checked)}
-            handleModuleChange={(module, checked) => {
-              if (checked) {
-                setCreateModules([...createModules, module]);
-              } else {
-                setCreateModules(createModules.filter(m => m !== module));
-              }
-            }}
-            handleDifficultyChange={(difficulty, checked) => {
-              if (checked) {
-                setCreateDifficulties([...createDifficulties, difficulty]);
-              } else {
-                setCreateDifficulties(
-                  createDifficulties.filter(d => d !== difficulty)
-                );
-              }
-            }}
+            handleChange={handleSelectionChange}
           />
         )}
         {step === 2 && aplusCourse && (
           <CreateAplusAttainments
             aplusCourseId={aplusCourse.id}
-            createFullPoints={createFullPoints}
-            createModules={createModules}
-            createDifficulties={createDifficulties}
+            attainmentsWithSource={attainmentsWithSource}
           />
         )}
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose}>Cancel</Button>
-        <Button
-          disabled={step === 0}
-          onClick={() => setStep(2)}
-        >
-          Next
-        </Button>
+        {step > 0 && (
+          <Button onClick={() => setStep(step - 1)} sx={{mr: 'auto'}}>
+            Back
+          </Button>
+        )}
+        {step <= 1 && (
+          <Button disabled={step === 0} onClick={() => setStep(step + 1)}>
+            Next
+          </Button>
+        )}
+        {step === 2 && (
+          <Button onClick={() => handleClose()} X>
+            Submit
+          </Button>
+        )}
       </DialogActions>
     </Dialog>
   );
---
+++ client/src/components/course-view/SelectAplusGradeSources.tsx
@@ -0,0 +1,121 @@
+// SPDX-FileCopyrightText: 2024 The Aalto Grades Developers
+//
+// SPDX-License-Identifier: MIT
+
+import {ArrowDropDown} from '@mui/icons-material';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+} from '@mui/material';
+
+import {
+  AplusCourseData,
+  AplusGradeSourceData,
+  AplusGradeSourceType,
+} from '@/common/types';
+import {useFetchAplusExerciseData} from '../../hooks/useApi';
+
+type PropsType = {
+  aplusCourse: AplusCourseData;
+  handleChange: (
+    checked: boolean,
+    name: string,
+    source: AplusGradeSourceData
+  ) => void;
+};
+
+const SelectAplusGradeSources = ({
+  aplusCourse,
+  handleChange,
+}: PropsType): JSX.Element => {
+  const aplusExerciseData = useFetchAplusExerciseData(aplusCourse.id);
+
+  return (
+    <>
+      {aplusExerciseData.data && (
+        <>
+          <FormControlLabel
+            control={
+              <Checkbox
+                onChange={e =>
+                  handleChange(e.target.checked, 'A+ Course', {
+                    attainmentId: -1,
+                    aplusCourseId: aplusCourse.id,
+                    sourceType: AplusGradeSourceType.FullPoints,
+                  })
+                }
+              />
+            }
+            label="Full points"
+          />
+          <Accordion>
+            <AccordionSummary expandIcon={<ArrowDropDown />}>
+              Modules
+            </AccordionSummary>
+            <AccordionDetails>
+              <FormGroup>
+                {aplusExerciseData.data.modules.map(module => (
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        onChange={e =>
+                          handleChange(
+                            e.target.checked,
+                            `A+ Module: ${module.name}`,
+                            {
+                              attainmentId: -1,
+                              aplusCourseId: aplusCourse.id,
+                              sourceType: AplusGradeSourceType.Module,
+                              moduleId: module.id,
+                            }
+                          )
+                        }
+                      />
+                    }
+                    label={module.name}
+                  />
+                ))}
+              </FormGroup>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary expandIcon={<ArrowDropDown />}>
+              Difficulties
+            </AccordionSummary>
+            <AccordionDetails>
+              <FormGroup>
+                {aplusExerciseData.data.difficulties.map(difficulty => (
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        onChange={e =>
+                          handleChange(
+                            e.target.checked,
+                            `A+ Difficulty: ${difficulty}`,
+                            {
+                              attainmentId: -1,
+                              aplusCourseId: aplusCourse.id,
+                              sourceType: AplusGradeSourceType.Difficulty,
+                              difficulty: difficulty,
+                            }
+                          )
+                        }
+                      />
+                    }
+                    label={difficulty}
+                  />
+                ))}
+              </FormGroup>
+            </AccordionDetails>
+          </Accordion>
+        </>
+      )}
+    </>
+  );
+};
+
+export default SelectAplusGradeSources;
