9a149866b29d7cd6749209d4c3f75faf79c534b9
no-comma <82644961+no-comma@users.noreply.github.com>
2024-06-10 12:47:13 +0300

    Display A+ grade sources of course part in table

--- client/src/components/course-view/ViewAplusGradeSourcesDialog.tsx
+++ client/src/components/course-view/ViewAplusGradeSourcesDialog.tsx
@@ -8,11 +8,18 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
-  Typography,
+  Link,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
 } from '@mui/material';
 import {JSX} from 'react';
 
-import {AplusGradeSourceData} from '@/common/types';
+import {AplusGradeSourceData, AplusGradeSourceType} from '@/common/types';
 
 type PropsType = {
   handleClose: () => void;
@@ -25,14 +32,49 @@ const ViewAplusGradeSourcesDialog = ({
   open,
   aplusGradeSources,
 }: PropsType): JSX.Element => {
+  const getSourceName = (source: AplusGradeSourceData): string => {
+    switch (source.sourceType) {
+      case AplusGradeSourceType.FullPoints:
+        return 'Full Points';
+      case AplusGradeSourceType.Module:
+        return source.moduleName;
+      case AplusGradeSourceType.Difficulty:
+        return source.difficulty;
+    }
+  };
+
   return (
     <Dialog open={open} onClose={handleClose}>
       <DialogTitle>A+ grade sources</DialogTitle>
       <DialogContent>
-        {aplusGradeSources.map(source => (
-          // TODO: Actual design
-          <Typography>{JSON.stringify(source)}</Typography>
-        ))}
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Course Code</TableCell>
+                <TableCell>Course Name</TableCell>
+                <TableCell>Course Instance</TableCell>
+                <TableCell>Course URL</TableCell>
+                <TableCell>Source Name</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {aplusGradeSources.map(source => (
+                <TableRow>
+                  <TableCell>{source.aplusCourse.courseCode}</TableCell>
+                  <TableCell>{source.aplusCourse.name}</TableCell>
+                  <TableCell>{source.aplusCourse.instance}</TableCell>
+                  <TableCell>
+                    <Link href={source.aplusCourse.url} target="_blank">
+                      {source.aplusCourse.url}
+                    </Link>
+                  </TableCell>
+                  <TableCell>{getSourceName(source)}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose}>Close</Button>
