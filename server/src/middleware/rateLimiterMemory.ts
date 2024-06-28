d218c04bf3040255aa9c52782b429a22ac7815d3
gubo97000 <gubo97000@hotmail.it>
2024-06-28 17:36:47 +0300

    ip memory limiter

---
+++ server/src/middleware/rateLimiterMemory.ts
@@ -0,0 +1,29 @@
+import {NextFunction, Response} from 'express';
+import {RateLimiterMemory} from 'rate-limiter-flexible';
+import {TypedRequestBody} from 'zod-express-middleware';
+
+import {LoginDataSchema} from '@/common/types/auth';
+
+const rateLimiter = new RateLimiterMemory({
+  keyPrefix: 'ip_',
+  points: 5, // 10 requests
+  duration: 30, // per 1 second by IP
+  blockDuration: 15,
+});
+
+export const rateLimiterMemoryMiddleware = (
+  req: TypedRequestBody<typeof LoginDataSchema>,
+  res: Response,
+  next: NextFunction
+) => {
+  rateLimiter
+    .consume(req.ip ?? '')
+    .then(() => {
+      next();
+    })
+    .catch(() => {
+      return res.status(429).send({
+        errors: [`Try again in ${rateLimiter.blockDuration} seconds`],
+      });
+    });
+};
