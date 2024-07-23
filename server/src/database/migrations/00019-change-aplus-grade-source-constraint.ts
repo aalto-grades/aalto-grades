140a5b8ba49745f6cc7d1aec3eb22516fc3ac3e3
no-comma <82644961+no-comma@users.noreply.github.com>
2024-07-23 11:27:55 +0000

    Allow deleting course parts with A+ sources (#792)
    * Allow deleting course part with A+ sources
    
    * Fix year

---
+++ server/src/database/migrations/00019-change-aplus-grade-source-constraint.ts
@@ -0,0 +1,62 @@
+// SPDX-FileCopyrightText: 2024 The Aalto Grades Developers
+//
+// SPDX-License-Identifier: MIT
+
+/* eslint camelcase: off */
+
+import {QueryInterface} from 'sequelize';
+
+import logger from '../../configs/winston';
+
+export default {
+  up: async (queryInterface: QueryInterface): Promise<void> => {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      // RESTRICT -> CASCADE
+      await queryInterface.removeConstraint(
+        'aplus_grade_source',
+        'aplus_grade_source_attainment_id_fkey',
+        {transaction}
+      );
+      await queryInterface.addConstraint('aplus_grade_source', {
+        type: 'foreign key',
+        fields: ['course_part_id'],
+        name: 'aplus_grade_source_course_part_id_fkey',
+        references: {table: 'course_part', field: 'id'},
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        transaction,
+      });
+
+      await transaction.commit();
+    } catch (error) {
+      await transaction.rollback();
+      logger.error(error);
+    }
+  },
+  down: async (queryInterface: QueryInterface): Promise<void> => {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      // CASCADE -> RESTRICT
+      await queryInterface.removeConstraint(
+        'aplus_grade_source',
+        'aplus_grade_source_course_part_id_fkey',
+        {transaction}
+      );
+      await queryInterface.addConstraint('aplus_grade_source', {
+        type: 'foreign key',
+        fields: ['course_part_id'],
+        name: 'aplus_grade_source_attainment_id_fkey',
+        references: {table: 'course_part', field: 'id'},
+        onDelete: 'RESTRICT',
+        onUpdate: 'CASCADE',
+        transaction,
+      });
+
+      await transaction.commit();
+    } catch (error) {
+      await transaction.rollback();
+      logger.error(error);
+    }
+  },
+};
