8d20a4b2fdb4df452f785f269bb6af047939eca6
no-comma <82644961+no-comma@users.noreply.github.com>
2024-08-07 12:16:25 +0300

    Add comment to rename.sh

--- client/rename.sh
+++ client/rename.sh
@@ -4,6 +4,10 @@
 #
 # SPDX-License-Identifier: MIT
 
+# Used for renaming translation keys, could be improved to also change the
+# translation files. Breaks when the translation function uses interpolation or
+# is split across multiple lines.
+
 echo -n "Old: "
 read old
 echo -n "New: "
