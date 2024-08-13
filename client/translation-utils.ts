b37eb0c33b366598b113dad425922850c2d4ec83
no-comma <82644961+no-comma@users.noreply.github.com>
2024-08-13 10:07:29 +0300

    Fix updateTranslation

--- client/translation-utils.ts
+++ client/translation-utils.ts
@@ -164,38 +164,20 @@ function updateTranslation(file: File, changes: Change[]): void {
   }
   fs.copyFileSync(file, backup);
 
-  const original = JSON.parse(
-    fs.readFileSync(file).toString()
-  ) as TranslationJson;
-  const updated: TranslationJson = structuredClone(original);
+  const entries = parseFileEntries(file).entries;
 
-  function get(object: TranslationJson, key: string): string {
-    const keyParts = key.split('.');
-    let value: string | null = null;
-
-    let iter: TranslationJson = object;
-    keyParts.forEach((part, i) => {
-      if (i === keyParts.length - 1) {
-        value = iter[part] as string;
-      } else {
-        iter = iter[part] as TranslationJson;
-      }
-    });
-
-    return value!;
+  for (const change of changes) {
+    (entries.find(entry => entry.key === change.before) as Entry).key = change.after;
   }
 
-  function set(
-    object: TranslationJson,
-    key: string,
-    value: string | undefined
-  ): void {
-    const keyParts = key.split('.');
+  const object: TranslationJson = {};
+  for (const entry of entries) {
+    const keyParts = entry.key.split('.');
 
     let iter: TranslationJson = object;
     keyParts.forEach((part, i) => {
       if (i === keyParts.length - 1) {
-        iter[part] = value;
+        iter[part] = entry.value;
       } else {
         if (iter[part] === undefined) {
           iter[part] = {};
@@ -205,13 +187,7 @@ function updateTranslation(file: File, changes: Change[]): void {
     });
   }
 
-  for (const change of changes) {
-    const value = get(original, change.before);
-    set(updated, change.before, undefined);
-    set(updated, change.after, value);
-  }
-
-  fs.writeFileSync(file, JSON.stringify(updated, null, 2));
+  fs.writeFileSync(file, JSON.stringify(object, null, 2));
 }
 
 function rename(): void {
