0033fd330fd2fa6dbf6b53691ff830f759c83987
no-comma <82644961+no-comma@users.noreply.github.com>
2024-08-28 12:00:40 +0300

    Redirect when there are no grading models

--- client/src/components/course/CourseRedirect.tsx
+++ client/src/components/course/CourseRedirect.tsx
@@ -5,18 +5,30 @@
 import {JSX} from 'react';
 import {Navigate, useParams} from 'react-router-dom';
 
-import {useGetCourseParts} from '@/hooks/useApi';
+import {useGetCourseTasks} from '@/hooks/api/courseTask';
+import {useGetAllGradingModels, useGetCourseParts} from '@/hooks/useApi';
 
 const CourseRedirect = (): JSX.Element => {
   const {courseId} = useParams() as {courseId: string};
   const courseParts = useGetCourseParts(courseId);
+  const courseTasks = useGetCourseTasks(courseId);
+  const gradingModels = useGetAllGradingModels(courseId);
 
-  if (courseParts.data === undefined) return <></>;
+  if (
+    courseParts.data === undefined ||
+    courseTasks.data === undefined ||
+    gradingModels.data === undefined
+  )
+    return <></>;
 
   return (
     <>
-      {courseParts.data.length > 0 ? (
-        <Navigate to={`/${courseId}/course-results`} />
+      {courseParts.data.length > 0 && courseTasks.data.length > 0 ? (
+        gradingModels.data.length > 0 ? (
+          <Navigate to={`/${courseId}/course-results`} />
+        ) : (
+          <Navigate to={`/${courseId}/models`} />
+        )
       ) : (
         <Navigate to={`/${courseId}/course-parts`} />
       )}
