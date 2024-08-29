274efb9169b9c599a9223c324327b486d28c22a1
Juho Röyskö <juho.roysko@aalto.fi>
2024-08-29 16:01:50 +0300

    Add missing license

--- client/src/components/course/models-view/ModelButton.tsx
+++ client/src/components/course/models-view/ModelButton.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 The Aalto Grades Developers
+//
+// SPDX-License-Identifier: MIT
+
 import {Archive, Delete, Edit, Unarchive, Warning} from '@mui/icons-material';
 import {
   IconButton,
