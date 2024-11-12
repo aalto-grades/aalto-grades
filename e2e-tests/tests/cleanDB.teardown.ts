1586b59e953c70379e73e2e5644caf2909d42010
gubo97000 <gubo97000@hotmail.it>
2024-11-12 14:33:48 +0200

    license

--- e2e-tests/tests/cleanDB.teardown.ts
+++ e2e-tests/tests/cleanDB.teardown.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 The Aalto Grades Developers
+//
+// SPDX-License-Identifier: MIT
+
 import {test as teardown} from '@playwright/test';
 
 import {cleanDb} from './helper';
