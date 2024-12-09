bdfe26e7e54ab098502526e7a7888f46170216d6
trongduc2206 <64483353+trongduc2206@users.noreply.github.com>
2024-12-09 16:43:01 +0200

    modify the logic for detecting edited task grades (#935)
    modify the logic for detecting edited task grades
    
    ---------
    
    Co-authored-by: Vu Trong <vud3@t31300-lr036.org.aalto.fi>
    Co-authored-by: gubo97000 <gubo97000@hotmail.it>

--- client/src/components/course/course-results-view/table/EditGradesDialog.tsx
+++ client/src/components/course/course-results-view/table/EditGradesDialog.tsx
@@ -243,6 +243,20 @@ const EditGradesDialog = ({
     const deletedGrades: number[] = [];
     const editedGrades: {gradeId: number; data: EditTaskGradeData}[] = [];
 
+    /**
+     * Replacer function used in the JSON.stringify cxomparison, ignore the
+     * 'selected' property by returning undefined
+     */
+    const ignoreSelectedColumn = (
+      key: string,
+      value: ColTypes[keyof ColTypes]
+    ): ColTypes[keyof ColTypes] | undefined => {
+      if (key === 'selected') {
+        return undefined;
+      }
+      return value;
+    };
+
     for (const row of rows) {
       if (row.gradeId === -1) {
         newGrades.push({
@@ -254,24 +268,31 @@ const EditGradesDialog = ({
           expiryDate: row.expiryDate,
           comment: row.comment,
         });
-      } else {
-        editedGrades.push({
-          gradeId: row.gradeId,
-          data: {
-            grade: row.aplusGrade ? undefined : row.grade,
-            date: row.date,
-            expiryDate: row.expiryDate,
-            comment: row.comment,
-          },
-        });
       }
     }
 
     const rowIds = new Set(rows.map(row => row.gradeId));
     for (const initRow of initRows) {
-      if (!rowIds.has(initRow.gradeId)) deletedGrades.push(initRow.gradeId);
+      if (!rowIds.has(initRow.gradeId)) {
+        deletedGrades.push(initRow.gradeId);
+      } else {
+        const newRow = rows.find(row => row.gradeId === initRow.gradeId);
+        if (
+          JSON.stringify(newRow, ignoreSelectedColumn) !==
+          JSON.stringify(initRow, ignoreSelectedColumn)
+        ) {
+          editedGrades.push({
+            gradeId: newRow!.gradeId,
+            data: {
+              grade: newRow!.aplusGrade ? undefined : newRow!.grade,
+              date: newRow!.date,
+              expiryDate: newRow!.expiryDate,
+              comment: newRow!.comment,
+            },
+          });
+        }
+      }
     }
-
     await Promise.all([
       addGrades.mutateAsync(newGrades),
       ...deletedGrades.map(async gradeId => deleteGrade.mutateAsync(gradeId)),
