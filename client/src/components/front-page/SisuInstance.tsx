fc53b7629475d982bb9c171e1ee6c7b2ab8f034d
jes237 <jeres230792@gmail.com>
2025-02-17 17:39:54 +0200

    update instance view

--- client/src/components/front-page/SisuInstance.tsx
+++ client/src/components/front-page/SisuInstance.tsx
@@ -2,6 +2,7 @@
 //
 // SPDX-License-Identifier: MIT
 
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
 import {
   Box,
   Button,
@@ -13,7 +14,14 @@ import {
   Typography,
   styled,
 } from '@mui/material';
-import {Fragment, type JSX} from 'react';
+import MuiAccordion, {type AccordionProps} from '@mui/material/Accordion';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import MuiAccordionSummary, {
+  type AccordionSummaryProps,
+  accordionSummaryClasses,
+} from '@mui/material/AccordionSummary';
+import DOMPurify from 'dompurify';
+import {Fragment, type JSX, useState} from 'react';
 import {useTranslation} from 'react-i18next';
 
 import type {
@@ -24,7 +32,7 @@ import type {
 import {sisuLanguageOptions} from '@/utils';
 
 const StyledCard = styled(Card)(({theme}) => ({
-  padding: 5,
+  padding: theme.spacing(1),
   height: '100%',
   minHeight: '170px',
   display: 'flex',
@@ -38,6 +46,55 @@ const StyledCard = styled(Card)(({theme}) => ({
   },
 }));
 
+const Accordion = styled((props: AccordionProps) => (
+  <MuiAccordion sx={{mb: 1.5}} disableGutters elevation={0} square {...props} />
+))(({theme}) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  '&::before': {
+    display: 'none',
+  },
+}));
+
+const AccordionSummary = styled((props: AccordionSummaryProps) => (
+  <MuiAccordionSummary
+    expandIcon={
+      <Box
+        sx={{
+          width: 24,
+          height: 24,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: '50%',
+          backgroundColor: 'white',
+          border: '1px solid black',
+        }}
+      >
+        <ArrowForwardIosSharpIcon sx={{fontSize: '0.9rem'}} />
+      </Box>
+    }
+    {...props}
+  />
+))(({theme}) => ({
+  backgroundColor: 'rgba(0, 0, 0, .03)',
+  flexDirection: 'row-reverse',
+  [`& .${accordionSummaryClasses.expandIconWrapper}.${accordionSummaryClasses.expanded}`]:
+    {
+      transform: 'rotate(90deg)',
+    },
+  [`& .${accordionSummaryClasses.content}`]: {
+    marginLeft: theme.spacing(1),
+  },
+  ...theme.applyStyles('dark', {
+    backgroundColor: 'rgba(255, 255, 255, .05)',
+  }),
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({theme}) => ({
+  padding: theme.spacing(2),
+  borderTop: '1px solid rgba(0, 0, 0, .125)',
+}));
+
 type PropsType = {
   course: SisuCourseInstance;
   selectCourse: () => void;
@@ -45,10 +102,11 @@ type PropsType = {
 
 const SisuInstance = ({course, selectCourse}: PropsType): JSX.Element => {
   const {t, i18n} = useTranslation();
-  const localLang = i18n.language as keyof LocalizedString;
+  const [expanded, setExpanded] = useState<string | false>(false);
+
+  const uiLang = i18n.language as keyof LocalizedString;
   const instructionLang =
     course.languageOfInstructionCodes[0].toUpperCase() as Language;
-
   const otherLanguages = ['fi', 'en', 'sv'].filter(
     lang => lang !== i18n.language
   );
@@ -56,23 +114,64 @@ const SisuInstance = ({course, selectCourse}: PropsType): JSX.Element => {
   const getCourseLanguage = (): string => {
     const found = sisuLanguageOptions.find(lang => lang.id === instructionLang);
 
-    if (!found) {
-      return 'unknown';
-    }
+    if (!found) return '-';
 
-    return found.language[localLang];
+    return found.language[uiLang];
   };
+
+  const getContent = (content: string): string => {
+    return content.length > 0 ? content : t('general.no-content');
+  };
+
+  const getGrading = (value: string): string => {
+    switch (value) {
+      case '0-5':
+        return t('utils.scale-numerical');
+      case 'hyl-hyv':
+        return t('utils.scale-pass-fail');
+      case 'toinen-kotim':
+        return t('utils.second-lang');
+      default:
+        return value;
+    }
+  };
+
+  const handleChange =
+    (panel: string) => (_event: React.SyntheticEvent, newExpanded: boolean) => {
+      setExpanded(newExpanded ? panel : false);
+    };
 
   return (
     <StyledCard>
       <CardContent>
-        <Typography gutterBottom sx={{color: 'text.secondary', fontSize: 14}}>
-          {course.organizationName[localLang]}
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            width: '100%',
+            gap: {xs: 1, sm: 2},
+            flexDirection: {xs: 'column-reverse', sm: 'row'},
+          }}
+        >
+          <Typography
+            variant="subtitle2"
+            gutterBottom
+            sx={{color: 'text.secondary'}}
+          >
+            {course.organizationName[uiLang]}
+          </Typography>
+          <Typography
+            variant="subtitle2"
+            gutterBottom
+            sx={{color: 'text.secondary'}}
+          >
+            id: {course.id}
+          </Typography>
+        </Box>
+        <Typography variant="h6" component="div">
+          <b>{course.name[uiLang]}</b>
         </Typography>
-        <Typography variant="h5" component="div">
-          {course.name[localLang]}
-        </Typography>
-        <Typography sx={{color: 'text.secondary', mb: 1.5}}>
+        <Typography variant="subtitle2" sx={{color: 'text.secondary', mb: 1}}>
           {otherLanguages.map(lang => (
             <Fragment key={lang}>
               {lang}: {course.name[lang as keyof LocalizedString]}
@@ -80,16 +179,21 @@ const SisuInstance = ({course, selectCourse}: PropsType): JSX.Element => {
             </Fragment>
           ))}
         </Typography>
+        <Box>
+          <Typography variant="h6">
+            <b>{t('course.edit.sisu-search-basic-information')}:</b>
+          </Typography>
+        </Box>
         <Box
           sx={{
             display: 'flex',
             gap: {xs: 1, sm: 2},
-            my: 2,
+            my: 1,
             flexDirection: {xs: 'column', sm: 'row'},
           }}
         >
           <Box>
-            <Typography variant="body1">
+            <Typography variant="subtitle2">
               {t('course.edit.teachers-in-charge')}:
             </Typography>
           </Box>
@@ -97,6 +201,7 @@ const SisuInstance = ({course, selectCourse}: PropsType): JSX.Element => {
             sx={{
               display: 'flex',
               gap: 1,
+              flexWrap: 'wrap',
               flexDirection: {xs: 'column', sm: 'row'},
             }}
           >
@@ -108,21 +213,22 @@ const SisuInstance = ({course, selectCourse}: PropsType): JSX.Element => {
         <Box
           sx={{
             display: 'flex',
+            justifyContent: 'flex-start',
             gap: {xs: 1, sm: 2},
             flexDirection: {xs: 'column', sm: 'row'},
           }}
         >
           <Box>
-            <Typography variant="body1">
+            <Typography variant="subtitle2">
               {t('course.edit.language')}: {getCourseLanguage()}
               <br />
               {t('course.edit.grading-scale')}:{' '}
-              {course.summary.gradingScale[localLang]}
+              {getGrading(course.summary.gradingScale[uiLang])}
             </Typography>
           </Box>
           <Divider orientation="vertical" flexItem />
           <Box>
-            <Typography variant="body1">
+            <Typography variant="subtitle2">
               {t('course.edit.start-date')}: {course.startDate}
               <br />
               {t('course.edit.end-date')}: {course.endDate}
@@ -130,12 +236,89 @@ const SisuInstance = ({course, selectCourse}: PropsType): JSX.Element => {
           </Box>
           <Divider orientation="vertical" flexItem />
           <Box>
-            <Typography variant="body1">
+            <Typography variant="subtitle2">
               {t('course.edit.min-credits')}: {course.credits.min}
               <br />
               {t('course.edit.max-credits')}: {course.credits.max}
             </Typography>
           </Box>
+        </Box>
+        <Box sx={{mt: 2}}>
+          <Accordion
+            expanded={expanded === 'panel1'}
+            onChange={handleChange('panel1')}
+          >
+            <AccordionSummary
+              aria-controls="content-content"
+              id="content-header"
+            >
+              <Typography component="span">
+                {t('course.edit.sisu-search-content')}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography variant="body2">
+                <span
+                  // eslint-disable-next-line react/no-danger
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(
+                      getContent(course.summary.content[uiLang])
+                    ),
+                  }}
+                />
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion
+            expanded={expanded === 'panel2'}
+            onChange={handleChange('panel2')}
+          >
+            <AccordionSummary
+              aria-controls="additional-information-content"
+              id="additional-information-header"
+            >
+              <Typography component="span">
+                {t('course.edit.sisu-search-additional-information')}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography variant="body2">
+                <span
+                  // eslint-disable-next-line react/no-danger
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(
+                      getContent(course.summary.additionalInformation[uiLang])
+                    ),
+                  }}
+                />
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion
+            expanded={expanded === 'panel3'}
+            onChange={handleChange('panel3')}
+          >
+            <AccordionSummary
+              aria-controls="assesment-method-content"
+              id="assesment-method-header"
+            >
+              <Typography component="span">
+                {t('course.edit.sisu-search-assessment-methods')}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography variant="body2">
+                <span
+                  // eslint-disable-next-line react/no-danger
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(
+                      getContent(course.summary.assesmentMethods[uiLang])
+                    ),
+                  }}
+                />
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
         </Box>
       </CardContent>
       <CardActions>
