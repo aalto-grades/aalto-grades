dd5c819621df73b371102f6b519ad046d3ec667f
jes237 <jeres230792@gmail.com>
2025-02-21 10:48:25 +0200

    sisu mock data

---
+++ server/src/configs/sisu.ts
@@ -0,0 +1,266 @@
+export const apiMockData = [
+  {
+    id: 'aalto-CUR-191724-3109137',
+    code: 'CS-A1111',
+    startDate: '2025-02-18',
+    endDate: '2025-02-25',
+    type: 'exam-exam',
+    name: {
+      fi: 'Ohjelmoinnin peruskurssi Y1, Tentti (rästitentti 2024 kursseille)',
+      sv: 'Grundkurs i programmering Y1, Tentamen (för vår 2024 kurs)',
+      en: 'Basic Course in Programming Y1, Exam (retake exam for 2024 courses)',
+    },
+    summary: {
+      workload: {
+        fi: 'Luennot 20 h, itsenäinen opiskelu ja harjoitustehtävien tekeminen 100 h, tenttiin valmistautuminen ja osallistuminen 13 h.',
+        sv: 'Föreläsningar 20 h, övningar och material 100 h, tentamen (förberedelse och deltagande) 13 h.',
+        en: 'Lectures 20 h, exercises and self-study material 100 h, exam (preparation and participation) 13 h.',
+      },
+      prerequisites: {
+        fi: 'Tietokoneen ja yleisimpien sovellusten (esim. sähköposti) peruskäyttötaito. Ei esitietovaatimuksia ohjelmoinnista.',
+        sv: 'Grunderna i att använda en dator och de vanligaste tillämpningarna (t.ex. e-post). Inga förkunskaper i programmering.',
+        en: 'The ability to use a typical personal computers and basic applications such as e-mail. No prior programming knowledge is required.',
+      },
+      learningOutcomes: {
+        fi: 'Kurssin suoritettuasi osaat kirjoittaa pieniä tietokoneohjelmia esimerkiksi insinööritehtävissä tarvittaviin laskentasovelluksiin ja pienten tietomäärien muokkaamiseen. Ymmärrät myös muiden kirjoittamia vastaavia ohjelmia.',
+        sv: 'Efter avklarad kurs kan du skriva små datorprogram till exempel för att utföra beräkningar som behövs i ingenjörsvetenskaper samt för att bearbeta små mängder data. Du förstår också motsvarande program skrivna av andra.',
+        en: 'After taking this course, you can write small computer programs, for example, to perform calculations needed in engineering applications or to manipulate small amounts of data. You also understand corresponding programs written by others.',
+      },
+      literature: null,
+      languageOfInstruction: {
+        fi: 'fi',
+        sv: 'fi',
+        en: 'fi',
+      },
+      registration: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      homepage: {},
+      content: {
+        fi: 'Lausekielinen ohjelmointi Python-kielen avulla, rakenteinen ohjelmointi, ohjelmistosuunnittelun alkeet, perustietorakenteet. Hyvä ohjelmointityyli.',
+        sv: 'Grunderna i procedurorienterad programmering med Python-språket, strukturerad programmering, grunderna i programplanering, grundläggande datastrukturer. God programmeringsstil.',
+        en: 'Basics of procedural programming in Python, structured programming, basics of program design. Basic data structures. Good programming style.',
+      },
+      cefrLevel: '',
+      level: {
+        fi: 'basic-studies',
+        sv: 'basic-studies',
+        en: 'basic-studies',
+      },
+      teacherInCharge: ['Mike Mock', 'Filipa Fake'],
+      assesmentMethods: {
+        fi: 'Luennot, pakolliset harjoitustehtävät ja tentti. ',
+        sv: 'Föreläsningarna, obligatoriska hemuppgifter och tentamen.',
+        en: 'Lectures, mandatory assignments and exam.',
+      },
+      substitutes: {
+        fi: 'CS-A1113',
+        sv: 'CS-A1113',
+        en: 'CS-A1113',
+        courseUnits: [
+          [
+            {
+              courseUnitGroupId: 'aalto-OPINKOHD-1113036894',
+              credits: null,
+            },
+          ],
+          [
+            {
+              courseUnitGroupId: 'aalto-OPINKOHD-1137141103',
+              credits: null,
+            },
+          ],
+        ],
+      },
+      additionalInformation: {
+        fi: ' Opetuskieli : suomi Opetusperiodi : 2022-2023 Syksy I - II  2023-2024 Syksy I - II',
+        sv: ' Undervisningsspråket : finska Undervisningsperiod : 2022-2023 Höst I - II  2023-2024 Höst I - II',
+        en: ' Teaching Language : Finnish Teaching Period : 2022-2023 Autumn I - II  2023-2024 Autumn I - II',
+      },
+      gradingScale: {
+        fi: '0-5',
+        sv: '0-5',
+        en: '0-5',
+      },
+      teachingPeriod: {
+        fi: 'i : suomi',
+        en: 'i : suomi',
+        sv: 'i : suomi',
+      },
+    },
+    organizations: [
+      {
+        organisationId: 'aalto-3b3aa303-843a-4a29-97ca-29c45d53f923',
+        educationalInstitutionUrn: null,
+        roleUrn: 'urn:code:organisation-role:responsible-organisation',
+        share: 1,
+        validityPeriod: {},
+      },
+    ],
+    organizationId: 'T313',
+    organizationName: {
+      fi: 'Tietotekniikan laitos',
+      sv: 'Institutionen för datateknik',
+      en: 'Department of Computer Science',
+    },
+    credits: {
+      min: 5,
+      max: 5,
+    },
+    courseUnitId: 'otm-5d1a0873-2107-4b0d-86dd-c1ccff6601aa',
+    languageOfInstructionCodes: ['fi'],
+    teachers: ['Mike Mock', 'Filipa Fake'],
+    enrolmentStartDate: '2024-12-20',
+    enrolmentEndDate: '2025-02-11',
+    mincredits: '5',
+    studySubGroups: [
+      {
+        id: 'aalto-ssg-191724-3818-5-11111',
+        name: {
+          en: 'T02',
+          fi: 'T02',
+          sv: 'T02',
+        },
+        type: {
+          en: 'Exam',
+          fi: 'Tentti',
+          sv: 'Tentamen',
+        },
+      },
+    ],
+  },
+  {
+    id: 'aalto-CUR-191725-3109138',
+    code: 'CS-A1111',
+    startDate: '2025-05-20',
+    endDate: '2025-05-27',
+    type: 'exam-exam',
+    name: {
+      fi: 'Ohjelmoinnin peruskurssi Y1, Tentti (rästitentti 2024 kursseille)',
+      sv: 'Grundkurs i programmering Y1, Tentamen (för 2024 kurs)',
+      en: 'Basic Course in Programming Y1, Exam (retake exam for 2024 courses)',
+    },
+    summary: {
+      workload: {
+        fi: 'Luennot 20 h, itsenäinen opiskelu ja harjoitustehtävien tekeminen 100 h, tenttiin valmistautuminen ja osallistuminen 13 h.',
+        sv: 'Föreläsningar 20 h, övningar och material 100 h, tentamen (förberedelse och deltagande) 13 h.',
+        en: 'Lectures 20 h, exercises and self-study material 100 h, exam (preparation and participation) 13 h.',
+      },
+      prerequisites: {
+        fi: 'Suositellut esitiedot: Tietokoneen ja yleisimpien sovellusten (esim. sähköposti) peruskäyttötaito. Ei esitietovaatimuksia ohjelmoinnista.',
+        sv: 'Rekommenderade förkunskaper: Grunderna i att använda en dator och de vanligaste tillämpningarna (t.ex. e-post). Inga förkunskaper i programmering.',
+        en: 'Recommended prerequisites: The ability to use a typical personal computers and basic applications such as e-mail. No prior programming knowledge is required.',
+      },
+      learningOutcomes: {
+        fi: 'Kurssin suoritettuasi osaat kirjoittaa pieniä tietokoneohjelmia esimerkiksi insinööritehtävissä tarvittaviin laskentasovelluksiin ja pienten tietomäärien muokkaamiseen. Ymmärrät myös muiden kirjoittamia vastaavia ohjelmia.',
+        sv: 'Efter avklarad kurs kan du skriva små datorprogram till exempel för att utföra beräkningar som behövs i ingenjörsvetenskaper samt för att bearbeta små mängder data. Du förstår också motsvarande program skrivna av andra.',
+        en: 'After taking this course, you can write small computer programs, for example, to perform calculations needed in engineering applications or to manipulate small amounts of data. You also understand corresponding programs written by others.',
+      },
+      literature: null,
+      languageOfInstruction: {
+        fi: 'fi',
+        sv: 'fi',
+        en: 'fi',
+      },
+      registration: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      homepage: {},
+      content: {
+        fi: 'Lausekielinen ohjelmointi Python-kielen avulla, rakenteinen ohjelmointi, ohjelmistosuunnittelun alkeet, perustietorakenteet. Hyvä ohjelmointityyli.',
+        sv: 'Grunderna i procedurorienterad programmering med Python-språket, strukturerad programmering, grunderna i programplanering, grundläggande datastrukturer. God programmeringsstil.',
+        en: 'Basics of procedural programming in Python, structured programming, basics of program design. Basic data structures. Good programming style.',
+      },
+      cefrLevel: '',
+      level: {
+        fi: 'basic-studies',
+        sv: 'basic-studies',
+        en: 'basic-studies',
+      },
+      teacherInCharge: ['Mike Mock', 'Filipa Fake'],
+      assesmentMethods: {
+        fi: 'Luennot, pakolliset harjoitustehtävät ja tentti. ',
+        sv: 'Föreläsningarna, obligatoriska hemuppgifter och tentamen.',
+        en: 'Lectures, mandatory assignments and exam.',
+      },
+      substitutes: {
+        fi: 'CS-A1113',
+        sv: 'CS-A1113',
+        en: 'CS-A1113',
+        courseUnits: [
+          [
+            {
+              courseUnitGroupId: 'aalto-OPINKOHD-1113036894',
+              credits: null,
+            },
+          ],
+          [
+            {
+              courseUnitGroupId: 'aalto-OPINKOHD-1137141103',
+              credits: null,
+            },
+          ],
+        ],
+      },
+      additionalInformation: {
+        fi: ' Opetuskieli: suomi Opetusperiodi: 2024-2025 Syksy I - II  2025-2026 Syksy I - II',
+        sv: ' Undervisningsspråket: finska Undervisningsperiod: 2024-2025 Höst I - II  2025-2026 Höst I - II',
+        en: ' Teaching Language: Finnish Teaching Period: 2024-2025 Autumn I - II  2025-2026 Autumn I - II',
+      },
+      gradingScale: {
+        fi: '0-5',
+        sv: '0-5',
+        en: '0-5',
+      },
+      teachingPeriod: {
+        fi: '2024-2025 Syksy I - II  2025-2026 Syksy I - II',
+        en: '2024-2025 Autumn I - II  2025-2026 Autumn I - II',
+        sv: '2024-2025 Höst I - II  2025-2026 Höst I - II',
+      },
+    },
+    organizations: [
+      {
+        organisationId: 'aalto-3b3aa303-843a-4a29-97ca-29c45d53f923',
+        educationalInstitutionUrn: null,
+        roleUrn: 'urn:code:organisation-role:responsible-organisation',
+        share: 1,
+        validityPeriod: {},
+      },
+    ],
+    organizationId: 'T313',
+    organizationName: {
+      fi: 'Tietotekniikan laitos',
+      sv: 'Institutionen för datateknik',
+      en: 'Department of Computer Science',
+    },
+    credits: {
+      min: 5,
+      max: 5,
+    },
+    courseUnitId: 'aalto-CU-1150973071-20240801',
+    languageOfInstructionCodes: ['fi'],
+    teachers: ['Mike Mock', 'Filipa Fake'],
+    enrolmentStartDate: '2025-03-21',
+    enrolmentEndDate: '2025-05-13',
+    mincredits: '5',
+    studySubGroups: [
+      {
+        id: 'aalto-ssg-191725-3818-5-11111',
+        name: {
+          en: 'T03',
+          fi: 'T03',
+          sv: 'T03',
+        },
+        type: {
+          en: 'Exam',
+          fi: 'Tentti',
+          sv: 'Tentamen',
+        },
+      },
+    ],
+  },
+];
