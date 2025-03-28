b922ff778ab2a0b703ebc27172be66892c84e582
jes237 <jeres230792@gmail.com>
2025-03-28 13:52:51 +0200

    group tests

--- e2e-tests/tests/admin.spec.ts
+++ e2e-tests/tests/admin.spec.ts
@@ -64,74 +64,84 @@ test.describe('Manage users as admin', () => {
 });
 
 test.describe('Test courses as admin', () => {
-  test('Check course', async ({page}) => {
-    await checkCourse(page);
+  test.describe('Course general', () => {
+    test('Check course', async ({page}) => {
+      await checkCourse(page);
+    });
+
+    test('Add course', async ({page}) => {
+      await createCourse(page);
+    });
+
+    test('Edit course', async ({page}) => {
+      await editCourse(page);
+    });
   });
 
-  test('Add course', async ({page}) => {
-    await createCourse(page);
+  test.describe('Course parts', () => {
+    test('View course parts', async ({page}) => {
+      await viewCourseParts(page);
+    });
+
+    test('Add course part', async ({page}) => {
+      await addCoursePart(page);
+    });
+
+    test('Edit course part', async ({page}) => {
+      await editCoursePart(page);
+    });
+
+    test('Archive and unarchive course part', async ({page}) => {
+      await archiveCoursePart(page);
+    });
   });
 
-  test('Edit course', async ({page}) => {
-    await editCourse(page);
-  });
-
-  test('View course parts', async ({page}) => {
-    await viewCourseParts(page);
-  });
-
-  test('Add course part', async ({page}) => {
-    await addCoursePart(page);
-  });
-
-  test('Edit course part', async ({page}) => {
-    await editCoursePart(page);
-  });
-
-  test('Archive and unarchive course part', async ({page}) => {
-    await archiveCoursePart(page);
-  });
-
-  test.describe('Add course part task', () => {
-    test('Success', async ({page}) => {
+  test.describe('Course part tasks', () => {
+    test('Add course part task - Success', async ({page}) => {
       await addCoursePartTaskSuccess(page);
     });
 
-    test('Fail (duplicate name)', async ({page}) => {
+    test('Add course part task - Fail', async ({page}) => {
       await addCoursePartTaskFail(page);
     });
   });
 
-  test('View grading model', async ({page}) => {
-    await viewGradingModel(page);
+  test.describe('Grading models', () => {
+    test('View grading model', async ({page}) => {
+      await viewGradingModel(page);
+    });
+
+    test('Create grading model', async ({page}) => {
+      await createGradingModel(page);
+    });
   });
 
-  test('Create grading model', async ({page}) => {
-    await createGradingModel(page);
+  test.describe('Download grades', () => {
+    test('CSV template', async ({page}) => {
+      await downloadCSVGradeTemplate(page);
+    });
+
+    test('Excel template', async ({page}) => {
+      await downloadExcelGradeTemplate(page);
+    });
   });
 
-  test('Download grades CSV template', async ({page}) => {
-    await downloadCSVGradeTemplate(page);
-  });
+  test.describe('Import grades', () => {
+    test('Using CSV file', async ({page}) => {
+      await importGradesWithFile(page, 'csv');
+    });
 
-  test('Download grades excel template', async ({page}) => {
-    await downloadExcelGradeTemplate(page);
-  });
+    test('Using Excel file', async ({page}) => {
+      await importGradesWithFile(page, 'xlsx');
+    });
 
-  test('Import grades using CSV file', async ({page}) => {
-    await importGradesWithFile(page, 'csv');
-  });
+    test('By pasting text', async ({page}) => {
+      await importGradesWithText(page);
+    });
 
-  test('Import grades using Excel file', async ({page}) => {
-    await importGradesWithFile(page, 'xlsx');
-  });
-
-  test('Import grades by pasting text', async ({page}) => {
-    await importGradesWithText(page);
-  });
-
-  test('Show warn dialog if backdrop click disabled', async ({page}) => {
-    await warnDialogIfBackdropClickDisabled(page);
+    test('Show warn dialog when backdrop click disabled', async ({page}) => {
+      await warnDialogIfBackdropClickDisabled(page);
+    });
   });
 
   test('Import course from Sisu', async ({page}) => {
